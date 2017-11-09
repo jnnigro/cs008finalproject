adb7111c4d9fc64f53b26c15a09fee1551871636
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-11-08 22:49:32 -0500

    Create sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,18 @@
+            <li>Final Project
+                <ol>
+                    <li><a href="finalproject/index.php">Home</a></li>
+                    <li><a href="finalproject/breakfast-all.php">Breakfast</a></li>
+                    <li><a href="finalproject/lunch-all.php">Lunch</a></li>
+                    <li><a href="finalproject/dinner-all.php">Dinner</a></li>
+                    <li><a href="finalproject/dessert-all.php">Dessert</a></li>
+                    <li><a href="finalproject/snacks-all.php">Snacks</a></li>
+                    <li><a href="finalproject/form.php">Order</a></li>
+                    <li>Project Files   
+                        <ol>
+                            <li><a href="finalproject/specs.pdf">Specification Document</a></li>
+                            <li><a href="css/custom3.css">Custom css</a></li>
+                            <li><a href="data/customorder.csv">Custom Order csv</a></li>                            
+                        </ol>
+                    </li>
+                </ol>                
+            </li>
