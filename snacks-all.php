cf62781510efc8de42ce18c3ef0ca42e4bb5a009
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-11-28 20:23:07 -0500

    Update snacks-all.php

--- snacks-all.php
+++ snacks-all.php
@@ -1,2 +1,111 @@
 <?php include 'top.php'; ?>
 <h1>All Snack Recipes</h1>
+
+<h2>Artichoke Dip</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>2 13oz. Cans of Artichoke Hearts in brine</li>
+    <li>3/4 Cup of Mayonnaise</li>
+    <li>3/4 Cup of Grated Parmesan Cheese</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions">Preheat oven to 250°F</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions">Drain and then roughly chop Artichoke hearts</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions">Mix Mayonnaise, Parmesan Cheese and chopped Artichoke Hearts in a medium sized bowl</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 4</h4>
+        <p class="directions">Spread mixture into oven-safe shallow casserole dish or pie plate</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 5</h4>
+        <p class="directions">Bake for 25-30 minutes until hot and bubbly</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 6</h4>
+        <p class="directions">Serve with bread or sturdy chips/crackers</p>
+    </li>
+</ul>
+
+<h2>Mickey Mouse Soft Pretzels</h2>
+
+<figure>
+    <img class="mickeypretzels" src="images/mickey-pretzels.jpg" alt="soft pretzels shaped like mickey mouse">
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>1 package Auntie Anne's Pretzels & More Homemade Baking Mix</li>
+    <li>Water</li>
+    <li>Flour for kneading</li>
+    <li>Melted butter (optional)</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions">Dissolve yeast in 1 1/4 cups warm water and let it sit for 2 minutes.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions">Add dough mix and mix until a ball forms.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions">Knead the dough ball on a lightly floured surface for 5-7 minutes. Add a small amount of flour if the dough is too sticky.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 4</h4>
+        <p class="directions">Place dough ball in large greased bowl and cover. Let it rise for 30 minutes in a warm spot. While it's rising, preheat the oven to 425°F.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 5</h4>
+        <p class="directions">Turn out the dough and cut it into strips. Take a strip and roll, stretch, and pull the dough into a rope. Then shape it into a circle (Mickey's face). Take two smaller pieces, make ropes, and shape them into smaller circles to make Mickey's ears. Place them on the larger circle and press on them to make sure they're attached.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 6</h4>
+        <p class="directions">Dissolve the baking soda into 4 cups of very hot water.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 7</h4>
+        <p class="directions">Dunk the pretzels in the baking soda solution, then place them on a greased baking sheet.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 8</h4>
+        <p class="directions">
+
+Add salt. If you want to make cinnamon sugar Mickey pretzels, skip this step.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 9</h4>
+        <p class="directions">Bake pretzels for 5 minutes, then, turn the tray 180° and bake for another 2-5 minutes until the pretzels are golden brown.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 10</h4>
+        <p class="directions">For salted pretzels, brush melted butter onto the pretzels. For cinnamon sugar pretzels, brush pretzels with melted butter, then sprinkle on the cinnamon sugar. Enjoy!</p>
+    </li>
+</ul>
+    
+<?php include 'footer.php'; ?>
