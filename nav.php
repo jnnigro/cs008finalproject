607e43c8e13db9437cafb1a864bb68d4de3e26e0
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-11-29 17:41:44 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,54 +2,54 @@
     <section class="navbar">
         <a href="index.php">Home</a>
         <section class="dropdown">
-            <a class="meal" href="allbreakfast.php">Breakfast 
+            <a class="meal" href="breakfast-all.php">Breakfast 
                 <i class="fa fa-caret-down"></i>
             </a>
             <section class="dropdown-content">
-                <a href="basic.php">Basic</a>
-                <a href="savory-breakfast.php">Savory</a>
-                <a href="sweet-breakfast.php">Sweet</a>
+                <a href="breakfast-basic.php">Basic</a>
+                <a href="breakfast-savory.php">Savory</a>
+                <a href="breakfast-sweet.php">Sweet</a>
             </section>
         </section>
         <section class="dropdown">
-            <a class="meal" href="alllunch.php">Lunch
+            <a class="meal" href="lunch-all.php">Lunch
                 <i class="fa fa-caret-down"></i>
             </a>
             <section class="dropdown-content">
-                <a href="soup.php">Soup</a>
-                <a href="sandwich.php">Sandwich</a>
-                <a href="salad.php">Salad</a>
+                <a href="lunch-soup.php">Soup</a>
+                <a href="lunch-sandwich.php">Sandwich</a>
+                <a href="lunch-salad.php">Salad</a>
             </section>
         </section>
         <section class="dropdown">
-            <a class="meal" href="allsnacks.php">Snacks 
+            <a class="meal" href="snacks-all.php">Snacks 
                 <i class="fa fa-caret-down"></i>
             </a>
             <section class="dropdown-content">
-                <a href="healthy-snack.php">Healthy</a>
-                <a href="savory-snack.php">Savory</a>
-                <a href="sweet-snack.php">Sweet</a>
+                <a href="snacks-healthy.php">Healthy</a>
+                <a href="snacks-savory.php">Savory</a>
+                <a href="snacks-sweet.php">Sweet</a>
             </section>
         </section>
         <section class="dropdown">
-            <a class="meal" href="alldinner.php">Dinner 
+            <a class="meal" href="dinner-all.php">Dinner 
                 <i class="fa fa-caret-down"></i>
             </a>
             <section class="dropdown-content">
-                <a href="30minute.php">30minute</a>
-                <a href="healthy-dinner.php">Healthy</a>
-                <a href="pizza.php">Comfort</a>
+                <a href="dinner-30minute.php">30 minute</a>
+                <a href="dinner-healthy.php">Healthy</a>
+                <a href="dinner-comfort.php">Comfort</a>
             </section>
         </section>
         <section class="dropdown">
-            <a class="meal" href="alldessert.php">Dessert</a>             
+            <a class="meal" href="dessert-all.php">Dessert</a>             
             <section class="dropdown-content">
-                <a href="cookies.php">Cookies</a>
-                <a href="cupcakes.php">Cupcakes</a>
-                <a href="seasonal.php">Seasonal</a>
+                <a href="dessert-cookies.php">Cookies</a>
+                <a href="dessert-cupcakes.php">Cupcakes</a>
+                <a href="dessert-seasonal.php">Seasonal</a>
             </section>
         </section>  
 
-        <a href="form.php">Join</a>
+        <a href="form.php">Cake Orders</a>
     </section>
 </nav>
