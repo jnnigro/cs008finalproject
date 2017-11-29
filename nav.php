cc48b543af35a12fd788e0ce6a5f10073de1d1a8
bennettcazayoux <33701275+bennettcazayoux@users.noreply.github.com>
2017-11-29 15:09:28 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,63 +1,55 @@
-<div>
-<nav id="nav" class="navcenter-parent">
-    <ul class="navcenter">
-        <?php
-        print '<li class="home';
-        if ($path_parts['filename'] == "home") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="index.php">Home</a>';
-        print '</li>';
+<nav>
+    <section class="navbar">
+        <a href="index.php">Home</a>
+        <section class="dropdown">
+            <a class="meal" href="allbreakfast.php">Breakfast 
+                <i class="fa fa-caret-down"></i>
+            </a>
+            <section class="dropdown-content">
+                <a href="basic.php">Basic</a>
+                <a href="savory-breakfast.php">Savory</a>
+                <a href="sweet-breakfast.php">Sweet</a>
+            </section>
+        </section>
+        <section class="dropdown">
+            <a class="meal" href="alllunch.php">Lunch
+                <i class="fa fa-caret-down"></i>
+            </a>
+            <section class="dropdown-content">
+                <a href="soup.php">Soup</a>
+                <a href="sandwich.php">Sandwich</a>
+                <a href="salad.php">Salad</a>
+            </section>
+        </section>
+        <section class="dropdown">
+            <a class="meal" href="allsnacks.php">Snacks 
+                <i class="fa fa-caret-down"></i>
+            </a>
+            <section class="dropdown-content">
+                <a href="healthy-snack.php">Healthy</a>
+                <a href="savory-snack.php">Savory</a>
+                <a href="sweet-snack.php">Sweet</a>
+            </section>
+        </section>
+        <section class="dropdown">
+            <a class="meal" href="alldinner.php">Dinner 
+                <i class="fa fa-caret-down"></i>
+            </a>
+            <section class="dropdown-content">
+                <a href="30minute.php">30minute</a>
+                <a href="healthy-dinner.php">Healthy</a>
+                <a href="pizza.php">Comfort</a>
+            </section>
+        </section>
+        <section class="dropdown">
+            <a class="meal" href="alldessert.php">Dessert</a>             
+            <section class="dropdown-content">
+                <a href="cookies.php">Cookies</a>
+                <a href="cupcakes.php">Cupcakes</a>
+                <a href="seasonal.php">Seasonal</a>
+            </section>
+        </section>  
 
-        print '<li class="';
-        if ($path_parts['filename'] == "breakfast") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="breakfast-all.php">Breakfast</a>';
-        print '</li>';
-        
-        print '<li class="';
-        if ($path_parts['filename'] == "lunch") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="lunch-all.php">Lunch</a>';
-        print '</li>';
-        
-                print '<li class="';
-        if ($path_parts['filename'] == "dinner") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="dinner-all.php">Dinner</a>';
-        print '</li>';
-
-        print '<li class="';
-        if ($path_parts['filename'] == "dessert") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="dessert-all.php">Dessert</a>';
-        print '</li>';
-        
-        print '<li class="';
-        if ($path_parts['filename'] == "snacks") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="snacks-all.php">Snacks</a>';
-        print '</li>';
-        
-        print '<li class="order"';
-        if ($path_parts['filename'] == "order") {
-            print ' activePage ';
-        }
-        print '">';
-        print '<a href="form.php">Order</a>';
-        print '</li>';
-        ?>
-    </ul>
+        <a href="form.php">Join</a>
+    </section>
 </nav>
-</div>
