8e406ce8f2291de804f2e1fd116ff63afc148ee1
ertaylor <33701359+ertaylor@users.noreply.github.com>
2017-11-29 15:07:04 -0500

    Update breakfast-all.php

--- breakfast-all.php
+++ breakfast-all.php
@@ -1,3 +1,302 @@
 <?php include 'top.php'; ?>
 <h1>All Breakfast Recipes</h1>
 
+<h2>French Toast</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Eggs Sunny Side Up</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Oatmeal Bowl</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Chocolate Chip Pancakes</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Burrito Omelette</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>KKD Shiner</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Egg White Scramble</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Spinach and Mushroom Scramble</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Cinnamon Rolls</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Apple Pie Overnight Oats</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>PBJ Overnight Oats</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Banana Muffins</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+
+<h2>Smoothie Bowl</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+<?php include 'footer.php'; ?>
