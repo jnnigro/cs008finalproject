33f3f33471c5e584437fa0593ff7930cce830951
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-11-28 20:21:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
 <?php include 'top.php'; ?>
 <h1>Home Page</h1>
+
+<h2>RECIPE TITLE</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        PHOTO CREDIT HERE
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions"></p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions"></p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions"></p>
+    </li>
+</ul>
+
+<?php include 'footer.php'; ?>
