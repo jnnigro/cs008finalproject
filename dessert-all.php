8e31495cb2ac14a8f89edf959cd5dfc5cac61c2d
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-11-28 20:21:02 -0500

    Update dessert-all.php

--- dessert-all.php
+++ dessert-all.php
@@ -1,2 +1,280 @@
 <?php include 'top.php'; ?>
 <h1>All Dessert Recipes</h1>
+
+<h2>Fig Macarons</h2>
+
+<figure>
+    <img class="figmac" src="images/fig-macarons.JPG" alt="cookie, cream, chocolate">
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>300g blanched slivered almonds or almond flour</li>
+    <li>300g powdered sugar</li>
+    <li>300g granulated sugar</li>
+    <li>75g water</li>
+    <li>220g aged egg whites divided</li>
+    <li>1 cup chocolate chips</li>
+    <li>1 cup heavy cream</li>
+    <li>Fig jam</li>
+    <li>Gold shimmer dust</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions">If you’re using blanched slivered almonds, add them to a food processor with some of the powdered sugar and grind into a flour. Sift it as you go and keep grinding until you only have about a tablespoon left. If you’re starting with almond flour, there’s not need to grind it further. Add the almond flour and powdered sugar to a food processor and process until they’re combined. Alternatively, you can sift them together into a large bowl at least three times.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions">In a large bowl, add the first portion of aged egg whites and mix it together until it forms a dough-like paste. Cover with plastic wrap so it doesn’t dry out and set it aside. To age the egg whites, measure out the eggs in a container that’s wiped down with lemon juice or vinegar, cover with plastic wrap, poke holes in the top, and keep them in the fridge for at least a day (the longer the better). When it comes time to use them, make sure they return to room temperature before using them. Using aged egg whites will give you a smoother meringue and a smoother final product.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions">Add the granulated sugar and water to a small saucepan and use a candy thermometer to keep an eye on the temperature. Bring it to a boil on medium heat and stir to mix everything together. Every once in a while, wipe down the sides with a wet pastry brush to avoid crystallization on the sides of the saucepan. When the syrup reaches 115°C, add the second portion of egg whites to a stand mixer and whisk on medium/high speed.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 4</h4>
+        <p class="directions">When the syrup reaches 118°C, pour it very slowly into the egg whites. When the egg whites reach stiff peaks, stop whisking.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 5</h4>
+        <p class="directions">Add about a third of the meringue into the almond flour and sugar dough and mix with a spatula until well combined. Keep mixing in the meringue a third at a time. With the last third, fold around the mixture and through the middle. Fold until the batter falls off the spatula in ribbons and disappears back into the batter, in about 10 seconds.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 6</h4>
+        <p class="directions">Add the batter to a piping bag fitted with a large round tip. Don’t add too much to the piping bag at one time.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 7</h4>
+        <p class="directions">Add dots of batter to the corners of a baking tray and line with parchment paper. Pipe the batter in rounds about an inch and a half in diameter. Space them around a half an inch apart. Tap the tray a few times on the counter to knock out any air bubbles.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 8</h4>
+        <p class="directions">Let them sit for at least 30 minutes until a skin forms and they don’t feel sticky. Preheat the oven to 180°C or 355°F.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 9</h4>
+        <p class="directions">Bake for 12 minutes and let them cool completely before touching them.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 10</h4>
+        <p class="directions">To make the ganache, heat the heavy cream until almost boiling and pour over the chocolate chips. Wait a few minutes, then stir until smooth. Chill for 30 minutes or until it’s thickened enough to stay on the macaron. When it’s cool, add to a piping bag. Add the fig jam to a piping bag, making sure there aren’t any big pieces of fig.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 11</h4>
+        <p class="directions">Pipe the ganache and the fig jam on the macarons and sandwich them. Top with gold shimmer dust (optional) or drizzle melted chocolate on top. The macarons will keep in the fridge for a week or so in an airtight container. Enjoy!</p>
+    </li>
+</ul>
+
+<h2>Rose Macarons</h2>
+
+<figure>
+    <img class="rosemac" src="images/rose-macarons.jpg" alt="macaron with rose background">
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>117g powdered sugar</li>
+    <li>71 g almond flour</li>
+    <li>2 aged egg whites at room temperature</li>
+    <li>53g granulated sugar</li>
+    <li>1 stick butter</li>
+    <li>1 1/2 cups sifted powdered sugar</li>
+    <li>1 teaspoon vanilla extract</li>
+    <li>rose syrup to taste</li>
+    <li>2 tablespoons milk</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions">Using a digital scale, measure out 117 grams of powdered sugar and 71 grams of almond flour and add it to a food processor or a separate bowl. If using a food processor, pulse until everything is sifted together. If using only a sieve, sift everything through 3 times. When there’s just under 2 tablespoons left, discard those 2 tablespoons.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions">Preheat the oven to 350°F. For the cookie batter, wipe down your bowl with vinegar or lemon juice to ensure there is no fat or oil anywhere on the bowl. Then, add 2 aged egg whites, around 90 grams. To age your egg whites, make sure the bowl they are ageing in is also wiped down with vinegar or lemon juice. Separate the eggs, cover the whites with plastic wrap, poke some holes in it, and keep it in the refrigerator for 24 hours (and up to a week). Make sure the egg whites are room temperature before using, as they gain more volume at room temperature.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions">Add 53 grams of granulated sugar, and stir it with the whisk attachment before letting the stand mixer do the rest of the work.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 4</h4>
+        <p class="directions">Whisk the egg whites and sugar at medium speed for 2 minutes. Then, increase the speed to medium high and whisk for another 2 minutes. Finally, increase the speed to high and whisk for another 2 minutes until stiff peaks form.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 5</h4>
+        <p class="directions">Add the almond flour and powdered sugar mixture to the meringue, and using a rubber spatula, fold it into the egg whites in the technique called macaronage. Go around the sides and through the middle and repeat until the batter reaches the ribbon stage, sinking back into the rest of the batter after around 10 seconds.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 6</h4>
+        <p class="directions">Transfer the batter to a piping bag with a ¾ inch round tip. Line a baking sheet with parchment paper (this is very important, as no fats or oils should touch the cookies, so never use cooking spray), and to secure the parchment paper, pipe a small amount of batter on each corner and press down with the parchment paper. Pipe the batter in rounds ¾ inch in diameter, an inch apart on the baking sheet.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 7</h4>
+        <p class="directions">Tap the baking sheet against the counter 3 times, releasing any air bubbles. If the batter isn’t flat, flatten any points before baking. Bake at 350°F for 13 minutes, turning the sheet halfway through. Most recipes require the cookies to sit for at least 30 minutes, but with this recipe they can go right into the oven.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 8</h4>
+        <p class="directions">After taking the cookies out of the oven, let them cool completely and don’t touch them for at least 10 minutes, as they are very fragile.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 9</h4>
+        <p class="directions">To make the frosting, cream one stick of butter. Slowly add the sifted 1 1/2 cups powdered sugar with the mixer on its lowest setting. Add the vanilla and milk, then add rose syrup to taste. Rose syrup is very concentrated, so be careful when adding it. Taste as you go, and add less than you think you’ll need, as the flavors are stronger when you pipe on the frosting. Continue to beat the frosting until it reaches a desired texture, adding small amounts of milk if needed. Transfer to a piping bag with a small star tip. If you don’t have a star tip, you can use any small tip or no tip at all.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 10</h4>
+        <p class="directions">Using an offset spatula or a palette knife, gently slide under the cookies. They should come off easily. Pipe the buttercream onto a cookie, then sandwich it with another cookie. If you’d like, pipe a small swirl on the top for decoration.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 11</h4>
+        <p class="directions">These macarons will keep for 3-5 days in the refrigerator as long as they’re sandwiched, and will get an even softer texture. Enjoy!</p>
+    </li>
+</ul>
+
+<h2>Chocolate Cupcakes with Coconut Grass and Chocolate Covered Strawberry Footballs</h2>
+
+<figure>
+    <img class="footballcupcakes" src="images/football-cupcakes.JPG" alt="Chocolate Cupcakes with Coconut Grass and Chocolate Covered Strawberry Footballs">
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>100g all-purpose flour</li>
+    <li>20g cocoa powder</li>
+    <li>140g sugar</li>
+    <li>1 1/2 teaspoon baking powder</li>
+    <li>pinch salt</li>
+    <li>40g unsalted butter at room temperature</li>
+    <li>120ml milk</li>
+    <li>1 egg</li>
+    <li>1/4 tsp vanilla extract</li>
+    <li>1 stick butter</li>
+    <li>1 1/2 cup sifted powdered sugar</li>
+    <li>1 tsp vanilla extract</li>
+    <li>2 tbsp milk</li>
+    <li>12 strawberries</li>
+    <li>2/3 cup chocolate chips</li>
+    <li>1/2 cup white chocolate chips</li>
+    <li>2/3 cup shredded coconut</li>
+    <li>Green food dye</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions">Melt the chocolate chips in the microwave at 30 second intervals until smooth. Dip the strawberries in chocolate for a smooth, even coat. Place on a baking tray lined with parchment paper and chill for 15 minutes.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions">Melt the white chocolate chips in the same fashion and transfer to a piping bag. Snip the end and pipe laces on the strawberry “footballs.” Chill until it’s time to top the cupcakes.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions">Preheat the oven to 170°C or 325°F. Add the flour, cocoa powder, sugar, baking powder, salt and butter in a mixing bowl. Beat until everything is combined and has a sandy texture.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 4</h4>
+        <p class="directions">Whisk the milk, egg and vanilla extract together in a separate bowl, then slowly pour it into the dry ingredients and beat until everything is combined. Go in with a spatula to get anything at the bottom not mixed in and beat for another 20 seconds.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 5</h4>
+        <p class="directions">Spoon the batter into a lined cupcake tin until 2/3 full. Bake for 20-25 minutes until a toothpick inserted in the center comes out clean. Let the cupcakes cool slightly in the tin and transfer them to a wire rack to cool completely.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 6</h4>
+        <p class="directions">While the cupcakes are baking, add the shredded coconut to a bowl and add a few drops of green food dye. Stir the coconut until it’s all green.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 7</h4>
+        <p class="directions">While the cupcakes are cooling, prepare the frosting. Add the butter to a mixing bowl and beat it until it’s creamy. Reduce the speed of the mixer and slowly add the sifted powdered sugar. Once it looks more like frosting, add the vanilla. Gradually add the milk until the frosting is a desired texture.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 8</h4>
+        <p class="directions">When the cupcakes are completely cool, frost the cupcakes. You could do this with a piping bag or just with a palette knife. Immediately after frosting, dunk the cupcake frosting-side down into the coconut “grass.”</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 9</h4>
+        <p class="directions">Top with a chocolate covered strawberry “football” and enjoy!</p>
+    </li>
+</ul>
+
+<h2>Vegan Chai Cupcakes</h2>
+
+<figure>
+    <img class="veganchai" src="images/vegan-chai.jpg" alt="vegan chai cupcake with cinnamon stick and anise star">
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>1 1/2 cups unsweetened almond milk</li>
+    <li>2 bags chai</li>
+    <li>2 cups flour</li>
+    <li>1 cup powdered sugar</li>
+    <li>2 tsp baking soda</li>
+    <li>1 tsp ground cinnamon</li>
+    <li>1 tsp ground ginger</li>
+    <li>1/4 tsp ground nutmeg</li>
+    <li>1 tbsp apple cider vinegar</li>
+    <li>2 tsp vanilla extract</li>
+    <li>1/3 cup canola oil</li>
+    <li>2 sticks vegan butter</li>
+    <li>3 cups powdered sugar</li>
+    <li>1 tsp vanilla extract</li>
+    <li>2 tbsp unsweetened almond milk</li>
+    <li>1 tsp cinnamon</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul class="instructions">
+    <li>
+        <h4 class="instruction-number">Step 1</h4>
+        <p class="directions">Preheat the oven to 350°F. Heat the almond milk to almost boiling and steep 2 bags of chai in the almond milk for 10 minutes.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 2</h4>
+        <p class="directions">Add the apple cider vinegar to the chai and stir.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 3</h4>
+        <p class="directions">In a mixing bowl, add flour, powdered sugar, baking soda, cinnamon, ginger, and nutmeg, and mix it together.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 4</h4>
+        <p class="directions">Add canola oil and vanilla to the chai mixture and whisk it together.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 5</h4>
+        <p class="directions">Add the wet ingredients to the dry ingredients in the mixing bowl and mix until incorporated.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 6</h4>
+        <p class="directions">Scoop the batter into each cupcake liner and bake at 350°F for 20 minutes. When a toothpick comes out clean, they're done.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 7</h4>
+        <p class="directions">While the cupcakes are cooling, make the icing. Cream the vegan butter, and add 3 cups of powdered sugar, a little at a time. Add the vanilla extract, milk, and cinnamon, and continue to mix. Spoon the buttercream into a piping bag with your favorite tip (mine is the French tip). Pipe the frosting onto each cupcake when they are completely cool.</p>
+    </li>
+    <li>
+        <h4 class="instruction-number">Step 8</h4>
+        <p class="directions">Sift on some ground cinnamon, then if you're feeling extra fancy, decorate the cupcakes with star anise and a cinnamon stick. Enjoy!</p>
+    </li>
+</ul>
+
+<?php include 'footer.php'; ?>
