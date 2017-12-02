254dfd6f4fe92a282ba976dcc8289891f5382f28
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:20:39 -0500

    Update snacks-all.php

--- snacks-all.php
+++ snacks-all.php
@@ -1,111 +1,240 @@
 <?php include 'top.php'; ?>
 <h1>All Snack Recipes</h1>
 
-<h2>Artichoke Dip</h2>
+<article class='recipe-title'>
+    <h2>Artichoke Dip</h2>
 
-<figure>
-    <img class="artichokedip" src="images/artichoke-dip.jpg" alt="chip being dipped in artichoke dip">
-    <figcaption>
-        Ed Taylor
-    </figcaption>
-</figure>
+    <figure>
+        <img class="artichoke-dip" src="images/artichoke-dip.jpg" alt="chip being dipped in artichoke dip">
+        <figcaption>
+            Ed Taylor
+        </figcaption>
+    </figure>
 
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li>2 13oz. Cans of Artichoke Hearts in brine</li>
-    <li>3/4 Cup of Mayonnaise</li>
-    <li>3/4 Cup of Grated Parmesan Cheese</li>
-</ul>
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 13oz. Cans of Artichoke Hearts in brine</li>
+        <li>3/4 Cup of Mayonnaise</li>
+        <li>3/4 Cup of Grated Parmesan Cheese</li>
+    </ul>
 
-<h3>Instructions</h3>
-<ul class="instructions">
-    <li>
-        <h4 class="instruction-number">Step 1</h4>
-        <p class="directions">Preheat oven to 250°F.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 2</h4>
-        <p class="directions">Drain and then roughly chop Artichoke hearts.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 3</h4>
-        <p class="directions">Mix Mayonnaise, Parmesan Cheese and chopped Artichoke Hearts in a medium sized bowl.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 4</h4>
-        <p class="directions">Spread mixture into oven-safe shallow casserole dish or pie plate.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 5</h4>
-        <p class="directions">Bake for 25-30 minutes until hot and bubbly.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 6</h4>
-        <p class="directions">Serve with bread or sturdy chips/crackers.</p>
-    </li>
-</ul>
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 250°F.</li>
+        <li class="directions">Drain and then roughly chop Artichoke hearts.</li>
+        <li class="directions">Mix Mayonnaise, Parmesan Cheese and chopped Artichoke Hearts in a medium sized bowl.</li>
+        <li class="directions">Spread mixture into oven-safe shallow casserole dish or pie plate.</li>
+        <li class="directions">Bake for 25-30 minutes until hot and bubbly.</li>
+        <li class="directions">Serve with bread or sturdy chips/crackers.</li>
+    </ol>
+</article>
 
-<h2>Mickey Mouse Soft Pretzels</h2>
+<article class='recipe-title'>
+    <h2>Banana Chocolate Chip Muffins</h2>
 
-<figure>
-    <img class="mickeypretzels" src="images/mickey-pretzels.jpg" alt="soft pretzels shaped like mickey mouse">
-    <figcaption>
-        Jennifer Nigro
-    </figcaption>
-</figure>
+    <figure>
+        <img class="banana-muffins" src="images/banana-muffins.jpg" alt="tray of banana chocolate chip muffins held above more muffins">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
 
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li>1 package Auntie Anne's Pretzels & More Homemade Baking Mix</li>
-    <li>Water</li>
-    <li>Flour for kneading</li>
-    <li>Melted butter (optional)</li>
-</ul>
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 1/2 cups all-purpose flour</li>
+        <li>1 cup sugar</li>
+        <li>1 tsp baking soda</li>
+        <li>1/2 tsp salt</li>
+        <li>3 medium ripe bananas</li>
+        <li>1 large egg</li>
+        <li>1/3 cup vegetable oil</li>
+        <li>1 tsp vanilla extract</li>
+    </ul>
 
-<h3>Instructions</h3>
-<ul class="instructions">
-    <li>
-        <h4 class="instruction-number">Step 1</h4>
-        <p class="directions">Dissolve yeast in 1 1/4 cups warm water and let it sit for 2 minutes.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 2</h4>
-        <p class="directions">Add dough mix and mix until a ball forms.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 3</h4>
-        <p class="directions">Knead the dough ball on a lightly floured surface for 5-7 minutes. Add a small amount of flour if the dough is too sticky.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 4</h4>
-        <p class="directions">Place dough ball in large greased bowl and cover. Let it rise for 30 minutes in a warm spot. While it's rising, preheat the oven to 425°F.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 5</h4>
-        <p class="directions">Turn out the dough and cut it into strips. Take a strip and roll, stretch, and pull the dough into a rope. Then shape it into a circle (Mickey's face). Take two smaller pieces, make ropes, and shape them into smaller circles to make Mickey's ears. Place them on the larger circle and press on them to make sure they're attached.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 6</h4>
-        <p class="directions">Dissolve the baking soda into 4 cups of very hot water.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 7</h4>
-        <p class="directions">Dunk the pretzels in the baking soda solution, then place them on a greased baking sheet.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 8</h4>
-        <p class="directions">
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">In a large bowl, combine dry ingredients. In another bowl, mash the bananas. Add egg, oil and vanilla and mix well. Stir into the dry ingredients just until combined.</li>
+        <li class="directions">Line a muffin tin and fill cups halfway.</li>
+        <li class="directions">Bake at 375°F for 18-22 minutes or until a toothpick inserted in the center comes out clean. Cool for 10 minutes in the pan, then transfer to a wire rack to cool completely.</li>
+    </ol>
+</article>
 
-Add salt. If you want to make cinnamon sugar Mickey pretzels, skip this step.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 9</h4>
-        <p class="directions">Bake pretzels for 5 minutes, then, turn the tray 180° and bake for another 2-5 minutes until the pretzels are golden brown.</p>
-    </li>
-    <li>
-        <h4 class="instruction-number">Step 10</h4>
-        <p class="directions">For salted pretzels, brush melted butter onto the pretzels. For cinnamon sugar pretzels, brush pretzels with melted butter, then sprinkle on the cinnamon sugar. Enjoy!</p>
-    </li>
-</ul>
-    
+<article class='recipe-title'>
+    <h2>Irish Soda Bread</h2>
+
+    <figure>
+        <img class="soda-bread" src="images/soda-bread.jpg" alt="loaf of Irish soda bread">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>450g strong white flour</li>
+        <li>1 tsp baking soda</li>
+        <li>1 tsp salt</li>
+        <li>300ml buttermilk or 150ml milk mixed with 150ml plain yogurt</li>
+        <li>6 tbsp tepid water</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat the oven to 200°C (400°F). Lightly grease a baking sheet.</li>
+        <li class="directions">Mix together the dry ingredients. Add the buttermilk and enough tepid water to form a very soft dough. I've found using your hands is the easiest way to do this.</li>
+        <li class="directions">Turn out the dough onto a lightly floured surface and shape into a round loaf around 7in in diameter.</li>
+        <li class="directions">Transfer the loaf onto the baking sheet and cut a shallow cross on the top with a sharp knife.</li>
+        <li class="directions">Bake for 30 minutes. After 30 minutes, Turn the loaf upside down and bake for another 10-15 minutes. When it's done, it will sound hollow when you tap it. Cool on a wire rack.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Joe's Hot Pretzels</h2>
+
+    <figure>
+        <img class="hot-pretzels" src="images/hot-pretzels.JPG" alt="hot and spicy pretzels">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 large bag pretzels</li>
+        <li>1/2 cup olive oil</li>
+        <li>1/2 package Hidden Valley Ranch (dry)</li>
+        <li>1 tsp cayenne pepper</li>
+        <li>1 tsp Chef Paul Blackened Steak Magic</li>
+        <li>1 1/2 tsp Mrs. Dash Garlic Herb</li>
+        <li>1/2 tsp dill</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Mix all ingredients in a large bowl.</li>
+        <li class="directions">Lay out pretzels on a baking tray lined with foil and bake at 200°F for 1 hour.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Mickey Mouse Soft Pretzels</h2>
+
+    <figure>
+        <img class="mickey-pretzels" src="images/mickey-pretzels.jpg" alt="soft pretzels shaped like mickey mouse">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 package Auntie Anne's Pretzels & More Homemade Baking Mix</li>
+        <li>Water</li>
+        <li>Flour for kneading</li>
+        <li>Melted butter (optional)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Dissolve yeast in 1 1/4 cups warm water and let it sit for 2 minutes.</li>
+        <li class="directions">Add dough mix and mix until a ball forms.</li>
+        <li class="directions">Knead the dough ball on a lightly floured surface for 5-7 minutes. Add a small amount of flour if the dough is too sticky.</li>
+        <li class="directions">Place dough ball in large greased bowl and cover. Let it rise for 30 minutes in a warm spot. While it's rising, preheat the oven to 425°F.</li>
+        <li class="directions">Turn out the dough and cut it into strips. Take a strip and roll, stretch, and pull the dough into a rope. Then shape it into a circle (Mickey's face). Take two smaller pieces, make ropes, and shape them into smaller circles to make Mickey's ears. Place them on the larger circle and press on them to make sure they're attached.</li>
+        <li class="directions">Dissolve the baking soda into 4 cups of very hot water.</li>
+        <li class="directions">Dunk the pretzels in the baking soda solution, then place them on a greased baking sheet.</li>
+        <li class="directions">Add salt. If you want to make cinnamon sugar Mickey pretzels, skip this step.</li>
+        <li class="directions">Bake pretzels for 5 minutes, then, turn the tray 180° and bake for another 2-5 minutes until the pretzels are golden brown.</li>
+        <li class="directions">For salted pretzels, brush melted butter onto the pretzels. For cinnamon sugar pretzels, brush pretzels with melted butter, then sprinkle on the cinnamon sugar. Enjoy!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Peanut Butter and Pumpkin Dog Biscuits</h2>
+
+    <figure>
+        <img class="dog-biscuits" src="images/dog-biscuits.jpg" alt="top left: round, star, and flower shaped biscuits. top right: biscuit in front of dog. bottom left: dog biting for biscuit. bottom right: dog eating biscuit.">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2/3 cup pumpkin puree</li>
+        <li>1/4 cup peanut butter</li>
+        <li>2 large eggs</li>
+        <li>3 cups flour</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 350°F. Line a baking sheet with parchment paper and set aside.</li>
+        <li class="directions">Beat pumpkin puree, peanut butter and eggs on medium-high until well combined, about 1-2 minutes. Gradually add 2 1/2 cups flour at low speed, beating just until incorporated. Add an additional 1/4 cup flour at a time just until the dough is no longer sticky.</li>
+        <li class="directions">On a lightly floured surface, knead the dough a few times until it comes together. Using a rolling pin, roll the dough to 1/4-inch thickness. Using cookie cutters, cut out desired shapes and place onto the baking sheet.</li>
+        <li class="directions">Bake 20-25 minutes until the edges are golden brown.</li>
+        <li class="directions">Let cool completely and enjoy a snack with your dog!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Rainbow Smoothie Pops</h2>
+
+    <figure>
+        <img class="smoothie-pops" src="images/rainbow-pops.jpg" alt="rainbow smoothie pops surrounded by fruit">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 1/2 cups plain or vanilla yogurt divided</li>
+        <li>1/2 cup strawberries</li>
+        <li>1/2 cup raspberries</li>
+        <li>1 cup oranges</li>
+        <li>1 cup pineapple</li>
+        <li>1 cup bananas</li>
+        <li>1 cup spinach</li>
+        <li>1 cup mixed berries</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">For the red layer: Add 1/2 cup yogurt, strawberries, and raspberries to a blender and blend until smooth. If you don't have a powerful blender and are using frozen fruit, chop them into smaller pieces before blending. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the orange layer: Add 1/2 cup yogurt and chopped oranges to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the yellow layer: Add 1/2 cup yogurt and chopped pineapple to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the green layer: Add 1/2 cup yogurt, bananas, and spinach to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the purple layer: Add 1/2 cup yogurt and mixed berries (I used a mix of raspberries, blackberries, and blueberries) to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">To assemble, spoon each color into each popsicle mold. I started with red and finished with purple, but you can go in any order. If you have molds with reusable caps with sticks, put them on and freeze the popsicles for 5 hours. If you're using wooden popsicle sticks, freeze for 1-2 hours, add the stick, and freeze for the remaining 3-4 hours.</li>
+        <li class="directions">After 5 hours, run the popsicle mold under warm water and carefully remove the popsicle. Enjoy!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Sweet and Savory Meatballs</h2>
+
+    <figure>
+        <img class="sweet-savory-meatballs" src="images/sweet-savory-meatballs.jpg" alt="plate of sweet and savory meatballs">
+        <figcaption>
+            Ed Taylor
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 can of jellied cranberry sauce</li>
+        <li>1 12oz bottle of chili sauce</li>
+        <li>1 small bag of Italian style frozen meatballs (can be homemade, but MUST be frozen)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Empty can of jellied cranberry sauce and bottle of chili sauce into medium sized saucepan, on medium-low heat, stirring frequently until cranberry sauce becomes becomes liquid.</li>
+        <li class="directions">Add frozen meatballs to sauce mixture and stir, coating meatballs evenly.</li>
+        <li class="directions">Let simmer until meatballs have thawed, and cooked through.</li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
 <?php include 'footer.php'; ?>
