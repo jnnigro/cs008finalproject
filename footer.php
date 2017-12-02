1dce29b2aa0235c3f121fb1b52cadb40c322f796
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:26:30 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer id="footer">
-    <p class="text-footer"><a href="#top">Top</a> | ©CS008 Final Project</p>
+    <p class="text-footer"><a href="#top">Top</a> | ©Cook, Bake, Eat</p>
 </footer>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a1f0a6d3d2b185f"></script>
 </body>
 </html>
