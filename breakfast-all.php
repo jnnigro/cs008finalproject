cbbd5daefb8a3473e31cdc831fbf725930b6f763
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:55:46 -0500

    Update breakfast-all.php

--- breakfast-all.php
+++ breakfast-all.php
@@ -1,219 +1,13 @@
 <?php include 'top.php'; ?>
 <h1>All Breakfast Recipes</h1>
 
-<h2>French Toast</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Eggs Sunny Side Up</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Oatmeal Bowl</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Chocolate Chip Pancakes</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Burrito Omelette</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>KKD Shiner</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Egg White Scramble</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Spinach and Mushroom Scramble</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
-<h2>Cinnamon Rolls</h2>
-
-<figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
-    <figcaption>
-        PHOTO CREDIT HERE
-    </figcaption>
-</figure>
-
-<h3>Ingredients</h3>
-<ul class="ingredients">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-
-<h3>Instructions</h3>
-<ol class="instructions">
-    <li class="directions"></li>
-    <li class="directions"></li>
-    <li class="directions"></li>
-</ol>
-
+<article class='recipe-title'>
 <h2>Apple Pie Overnight Oats</h2>
 
 <figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
+    <img class=“apple-oats” src="images/FILENAME.JPG" alt=“mason jar with apple and walnut overnight oats”>
     <figcaption>
-        PHOTO CREDIT HERE
+        Jennifer Nigro
     </figcaption>
 </figure>
 
@@ -230,13 +24,15 @@
     <li class="directions"></li>
     <li class="directions"></li>
 </ol>
+</article>
 
-<h2>PBJ Overnight Oats</h2>
+<article class='recipe-title'>
+<h2>Burrito Omelette</h2>
 
 <figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
+    <img class=“burrito-omelette” src="images/FILENAME.JPG" alt=“omelette with meat, beans, rice, and cheese”>
     <figcaption>
-        PHOTO CREDIT HERE
+        Bennett Cazayoux
     </figcaption>
 </figure>
 
@@ -253,13 +49,15 @@
     <li class="directions"></li>
     <li class="directions"></li>
 </ol>
+</article>
 
-<h2>Banana Muffins</h2>
+<article class='recipe-title'>
+<h2>Chocolate Chip Pancakes</h2>
 
 <figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
+    <img class=“choc-chip-pancakes” src="images/FILENAME.JPG" alt=“tray of chocolate chip pancakes“>
     <figcaption>
-        PHOTO CREDIT HERE
+        Jennifer Nigro
     </figcaption>
 </figure>
 
@@ -276,13 +74,15 @@
     <li class="directions"></li>
     <li class="directions"></li>
 </ol>
+</article>
 
-<h2>Smoothie Bowl</h2>
+<article class='recipe-title'>
+<h2>Cinnamon Rolls</h2>
 
 <figure>
-    <img class="" src="images/FILENAME.JPG" alt="">
+    <img class=“cinnamon-rolls” src="images/FILENAME.JPG" alt=“tray of cinnamon rolls”>
     <figcaption>
-        PHOTO CREDIT HERE
+        Jennifer Nigro
     </figcaption>
 </figure>
 
@@ -299,4 +99,231 @@
     <li class="directions"></li>
     <li class="directions"></li>
 </ol>
+</article>
+
+<article class='recipe-title'>
+<h2>Eggs Sunny Side Up</h2>
+
+<figure>
+    <img class=“sunny-eggs” src="images/FILENAME.JPG" alt=“eggs sunny side up served with toast and sautéed spinach”>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>French Toast</h2>
+
+<figure>
+    <img class=“french-toast” src="images/FILENAME.JPG" alt=“french toast served with fruit“>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>Copycat KKD Shiner</h2>
+
+<figure>
+    <img class=“kkd-shiner” src="images/FILENAME.JPG" alt=“Kountry Kart Deli Rise n’ Shiner”>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>PB&J Overnight Oats</h2>
+
+<figure>
+    <img class=“pbj-oats” src="images/FILENAME.JPG" alt=“mason jar with peanut butter and jelly overnight oats”>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>Mixed Berry Smoothie Bowl</h2>
+
+<figure>
+    <img class=“smoothie-bowl” src="images/FILENAME.JPG" alt=“mixed berry smoothie bowl topped with fruit and nut butter“>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>Oatmeal</h2>
+
+<figure>
+    <img class=“oatmeal” src="images/FILENAME.JPG" alt=“bowl of oatmeal topped with fruit, tea biscuits, and Nutella“>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>PB&J Overnight Oats</h2>
+
+<figure>
+    <img class=“pbj-oats” src="images/FILENAME.JPG" alt=“mason jar with peanut butter and jelly overnight oats”>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>Spinach and Corn Egg White Scramble</h2>
+
+<figure>
+    <img class=“egg-whites” src="images/FILENAME.JPG" alt=“spinach and corn egg white scramble”>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
+<article class='recipe-title'>
+<h2>Spinach and Mushroom Egg Scramble</h2>
+
+<figure>
+    <img class=“spinach-mushroom” src="images/FILENAME.JPG" alt=“spinach and mushroom egg scramble served with fruit”>
+    <figcaption>
+        Jennifer Nigro
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions"></li>
+    <li class="directions"></li>
+    <li class="directions"></li>
+</ol>
+</article>
+
 <?php include 'footer.php'; ?>
