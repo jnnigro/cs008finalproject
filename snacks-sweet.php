779ae8c12c59fea324881e140a5e7230153ddb33
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:22:52 -0500

    Update snacks-sweet.php

--- snacks-sweet.php
+++ snacks-sweet.php
@@ -1,1 +1,95 @@
 <?php include 'top.php'; ?>
+
+<h1>Sweet Snacks</h1>
+
+<article class='recipe-title'>
+    <h2>Banana Chocolate Chip Muffins</h2>
+
+    <figure>
+        <img class="banana-muffins" src="images/banana-muffins.jpg" alt="tray of banana chocolate chip muffins held above more muffins">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 1/2 cups all-purpose flour</li>
+        <li>1 cup sugar</li>
+        <li>1 tsp baking soda</li>
+        <li>1/2 tsp salt</li>
+        <li>3 medium ripe bananas</li>
+        <li>1 large egg</li>
+        <li>1/3 cup vegetable oil</li>
+        <li>1 tsp vanilla extract</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">In a large bowl, combine dry ingredients. In another bowl, mash the bananas. Add egg, oil and vanilla and mix well. Stir into the dry ingredients just until combined.</li>
+        <li class="directions">Line a muffin tin and fill cups halfway.</li>
+        <li class="directions">Bake at 375°F for 18-22 minutes or until a toothpick inserted in the center comes out clean. Cool for 10 minutes in the pan, then transfer to a wire rack to cool completely.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Rainbow Smoothie Pops</h2>
+
+    <figure>
+        <img class="smoothie-pops" src="images/rainbow-pops.jpg" alt="rainbow smoothie pops surrounded by fruit">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 1/2 cups plain or vanilla yogurt divided</li>
+        <li>1/2 cup strawberries</li>
+        <li>1/2 cup raspberries</li>
+        <li>1 cup oranges</li>
+        <li>1 cup pineapple</li>
+        <li>1 cup bananas</li>
+        <li>1 cup spinach</li>
+        <li>1 cup mixed berries</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">For the red layer: Add 1/2 cup yogurt, strawberries, and raspberries to a blender and blend until smooth. If you don't have a powerful blender and are using frozen fruit, chop them into smaller pieces before blending. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the orange layer: Add 1/2 cup yogurt and chopped oranges to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the yellow layer: Add 1/2 cup yogurt and chopped pineapple to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the green layer: Add 1/2 cup yogurt, bananas, and spinach to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the purple layer: Add 1/2 cup yogurt and mixed berries (I used a mix of raspberries, blackberries, and blueberries) to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">To assemble, spoon each color into each popsicle mold. I started with red and finished with purple, but you can go in any order. If you have molds with reusable caps with sticks, put them on and freeze the popsicles for 5 hours. If you're using wooden popsicle sticks, freeze for 1-2 hours, add the stick, and freeze for the remaining 3-4 hours.</li>
+        <li class="directions">After 5 hours, run the popsicle mold under warm water and carefully remove the popsicle. Enjoy!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Sweet and Savory Meatballs</h2>
+
+    <figure>
+        <img class="sweet-savory-meatballs" src="images/sweet-savory-meatballs.jpg" alt="plate of sweet and savory meatballs">
+        <figcaption>
+            Ed Taylor
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 can of jellied cranberry sauce</li>
+        <li>1 12oz bottle of chili sauce</li>
+        <li>1 small bag of Italian style frozen meatballs (can be homemade, but MUST be frozen)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Empty can of jellied cranberry sauce and bottle of chili sauce into medium sized saucepan, on medium-low heat, stirring frequently until cranberry sauce becomes becomes liquid.</li>
+        <li class="directions">Add frozen meatballs to sauce mixture and stir, coating meatballs evenly.</li>
+        <li class="directions">Let simmer until meatballs have thawed, and cooked through.</li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
+<?php include 'footer.php'; ?>
