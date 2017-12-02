3a2dd6dc78cbaafd7841ab08c4894ecb639df8dd
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:19:15 -0500

    Update dessert-cupcakes.php

--- dessert-cupcakes.php
+++ dessert-cupcakes.php
@@ -1,1 +1,176 @@
 <?php include 'top.php'; ?>
+
+<h1>Cupcakes</h1>
+
+<article class="recipe-title">
+    <h2>Vegan Chai Cupcakes</h2>
+
+    <figure>
+        <img class="vegan-chai" src="images/vegan-chai.jpg" alt="vegan chai cupcake with cinnamon stick and anise star">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 1/2 cups unsweetened almond milk</li>
+        <li>2 bags chai</li>
+        <li>2 cups flour</li>
+        <li>1 cup powdered sugar</li>
+        <li>2 tsp baking soda</li>
+        <li>1 tsp ground cinnamon</li>
+        <li>1 tsp ground ginger</li>
+        <li>1/4 tsp ground nutmeg</li>
+        <li>1 tbsp apple cider vinegar</li>
+        <li>2 tsp vanilla extract</li>
+        <li>1/3 cup Canola oil</li>
+        <li>2 sticks vegan butter</li>
+        <li>3 cups powdered sugar</li>
+        <li>1 tsp vanilla extract</li>
+        <li>2 tbsp unsweetened almond milk</li>
+        <li>1 tsp cinnamon</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat the oven to 350°F. Heat the almond milk to almost boiling and steep 2 bags of chai in the almond milk for 10 minutes.</li>
+        <li class="directions">Add the apple cider vinegar to the chai and stir.</li>
+        <li class="directions">In a mixing bowl, add flour, powdered sugar, baking soda, cinnamon, ginger, and nutmeg, and mix it together.</li>
+        <li class="directions">Add Canola oil and vanilla to the chai mixture and whisk it together.</li>
+        <li class="directions">Add the wet ingredients to the dry ingredients in the mixing bowl and mix until incorporated.</li>
+        <li class="directions">Scoop the batter into each cupcake liner and bake at 350°F for 20 minutes. When a toothpick comes out clean, they're done.</li>
+        <li class="directions">While the cupcakes are cooling, make the icing. Cream the vegan butter, and add 3 cups of powdered sugar, a little at a time. Add the vanilla extract, milk, and cinnamon, and continue to mix. Spoon the buttercream into a piping bag with your favorite tip (mine is the French tip). Pipe the frosting onto each cupcake when they are completely cool.</li>
+        <li class="directions">Sift on some ground cinnamon, then if you're feeling extra fancy, decorate the cupcakes with star anise and a cinnamon stick. Enjoy!</li>
+    </ol>
+</article>
+
+<article class="recipe-title">
+    <h2>Chocolate Cupcakes with Coconut Grass and Chocolate Covered Strawberry Footballs</h2>
+
+    <figure>
+        <img class="footballcupcakes" src="images/football-cupcakes.JPG" alt="Chocolate Cupcakes with Coconut Grass and Chocolate Covered Strawberry Footballs">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>100g all-purpose flour</li>
+        <li>20g cocoa powder</li>
+        <li>140g sugar</li>
+        <li>1 1/2 teaspoon baking powder</li>
+        <li>pinch salt</li>
+        <li>40g unsalted butter at room temperature</li>
+        <li>120ml milk</li>
+        <li>1 egg</li>
+        <li>1/4 tsp vanilla extract</li>
+        <li>1 stick butter</li>
+        <li>1 1/2 cup sifted powdered sugar</li>
+        <li>1 tsp vanilla extract</li>
+        <li>2 tbsp milk</li>
+        <li>12 strawberries</li>
+        <li>2/3 cup chocolate chips</li>
+        <li>1/2 cup white chocolate chips</li>
+        <li>2/3 cup shredded coconut</li>
+        <li>Green food dye</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Melt the chocolate chips in the microwave at 30 second intervals until smooth. Dip the strawberries in chocolate for a smooth, even coat. Place on a baking tray lined with parchment paper and chill for 15 minutes.</li>
+        <li class="directions">Melt the white chocolate chips in the same fashion and transfer to a piping bag. Snip the end and pipe laces on the strawberry “footballs.” Chill until it’s time to top the cupcakes.</li>
+        <li class="directions">Preheat the oven to 170°C or 325°F. Add the flour, cocoa powder, sugar, baking powder, salt and butter in a mixing bowl. Beat until everything is combined and has a sandy texture.</li>
+        <li class="directions">Whisk the milk, egg and vanilla extract together in a separate bowl, then slowly pour it into the dry ingredients and beat until everything is combined. Go in with a spatula to get anything at the bottom not mixed in and beat for another 20 seconds.</li>
+        <li class="directions">Spoon the batter into a lined cupcake tin until 2/3 full. Bake for 20-25 minutes until a toothpick inserted in the center comes out clean. Let the cupcakes cool slightly in the tin and transfer them to a wire rack to cool completely.</li>
+        <li class="directions">While the cupcakes are baking, add the shredded coconut to a bowl and add a few drops of green food dye. Stir the coconut until it’s all green.</li>
+        <li class="directions">While the cupcakes are cooling, prepare the frosting. Add the butter to a mixing bowl and beat it until it’s creamy. Reduce the speed of the mixer and slowly add the sifted powdered sugar. Once it looks more like frosting, add the vanilla. Gradually add the milk until the frosting is a desired texture.</li>
+        <li class="directions">When the cupcakes are completely cool, frost the cupcakes. You could do this with a piping bag or just with a palette knife. Immediately after frosting, dunk the cupcake frosting-side down into the coconut “grass.”</li>
+        <li class="directions">Top with a chocolate covered strawberry “football” and enjoy!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Funfetti Cupcake Hack</h2>
+
+    <figure>
+        <img class="cupcake-hack" src="images/cupcake-hack.jpg" alt="">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 box Funfetti cake mix (or any cake mix of your choosing)</li>
+        <li>4 large eggs</li>
+        <li>2/3 cup melted butter</li>
+        <li>1 cup milk </li>
+        <li>2 cups frosting</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Replace water 1:1 with milk (ideally whole milk, but you can use any kind you want)</li>
+        <li class="directions">Replace oil with melted butter and double the amount (1/2 cup becomes 1 cup, etc.)</li>
+        <li class="directions">Add an extra egg (or two if you want the cupcakes to be very rich, or if you’re using medium eggs)</li>
+        <li class="directions">Preheat oven to 350°F. Add cake mix, eggs, melted butter, and milk. Mix on low for 2 minutes.</li>
+        <li class="directions">Scoop batter into a lined cupcake tin so the cups are 3/4 full.</li>
+        <li class="directions">Bake for 20 minutes or until a toothpick is inserted and comes out clean. When the cupcakes are completely cook, frost and decorate.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Graveyard Cupcakes</h2>
+
+    <figure>
+        <img class="graveyard-cupcakes" src="images/graveyard-cupcake.JPG" alt="chocolate cupcake with shortbread gravestone in crushed cookie dirt">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Halloween Sprinkle-Filled Cupcakes</h2>
+
+    <figure>
+        <img class="sprinkle-cupcakes" src="images/sprinkles-cupcakes.JPG" alt="chococolate cupcakes filled with halloween sprinkles">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
+<?php include 'footer.php'; ?>
