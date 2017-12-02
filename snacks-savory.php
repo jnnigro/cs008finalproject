53fffb8f055c668c45c08b81b3a3cd42c2be2818
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:21:59 -0500

    Update snacks-savory.php

--- snacks-savory.php
+++ snacks-savory.php
@@ -1,1 +1,120 @@
 <?php include 'top.php'; ?>
+
+<h1>Savory Snacks</h1>
+
+<article class='recipe-title'>
+    <h2>Artichoke Dip</h2>
+
+    <figure>
+        <img class="artichoke-dip" src="images/artichoke-dip.jpg" alt="chip being dipped in artichoke dip">
+        <figcaption>
+            Ed Taylor
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 13oz. Cans of Artichoke Hearts in brine</li>
+        <li>3/4 Cup of Mayonnaise</li>
+        <li>3/4 Cup of Grated Parmesan Cheese</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 250°F.</li>
+        <li class="directions">Drain and then roughly chop Artichoke hearts.</li>
+        <li class="directions">Mix Mayonnaise, Parmesan Cheese and chopped Artichoke Hearts in a medium sized bowl.</li>
+        <li class="directions">Spread mixture into oven-safe shallow casserole dish or pie plate.</li>
+        <li class="directions">Bake for 25-30 minutes until hot and bubbly.</li>
+        <li class="directions">Serve with bread or sturdy chips/crackers.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Joe's Hot Pretzels</h2>
+
+    <figure>
+        <img class="hot-pretzels" src="images/hot-pretzels.JPG" alt="hot and spicy pretzels">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 large bag pretzels</li>
+        <li>1/2 cup olive oil</li>
+        <li>1/2 package Hidden Valley Ranch (dry)</li>
+        <li>1 tsp cayenne pepper</li>
+        <li>1 tsp Chef Paul Blackened Steak Magic</li>
+        <li>1 1/2 tsp Mrs. Dash Garlic Herb</li>
+        <li>1/2 tsp dill</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Mix all ingredients in a large bowl.</li>
+        <li class="directions">Lay out pretzels on a baking tray lined with foil and bake at 200°F for 1 hour.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Mickey Mouse Soft Pretzels</h2>
+
+    <figure>
+        <img class="mickey-pretzels" src="images/mickey-pretzels.jpg" alt="soft pretzels shaped like mickey mouse">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 package Auntie Anne's Pretzels & More Homemade Baking Mix</li>
+        <li>Water</li>
+        <li>Flour for kneading</li>
+        <li>Melted butter (optional)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Dissolve yeast in 1 1/4 cups warm water and let it sit for 2 minutes.</li>
+        <li class="directions">Add dough mix and mix until a ball forms.</li>
+        <li class="directions">Knead the dough ball on a lightly floured surface for 5-7 minutes. Add a small amount of flour if the dough is too sticky.</li>
+        <li class="directions">Place dough ball in large greased bowl and cover. Let it rise for 30 minutes in a warm spot. While it's rising, preheat the oven to 425°F.</li>
+        <li class="directions">Turn out the dough and cut it into strips. Take a strip and roll, stretch, and pull the dough into a rope. Then shape it into a circle (Mickey's face). Take two smaller pieces, make ropes, and shape them into smaller circles to make Mickey's ears. Place them on the larger circle and press on them to make sure they're attached.</li>
+        <li class="directions">Dissolve the baking soda into 4 cups of very hot water.</li>
+        <li class="directions">Dunk the pretzels in the baking soda solution, then place them on a greased baking sheet.</li>
+        <li class="directions">Add salt. If you want to make cinnamon sugar Mickey pretzels, skip this step.</li>
+        <li class="directions">Bake pretzels for 5 minutes, then, turn the tray 180° and bake for another 2-5 minutes until the pretzels are golden brown.</li>
+        <li class="directions">For salted pretzels, brush melted butter onto the pretzels. For cinnamon sugar pretzels, brush pretzels with melted butter, then sprinkle on the cinnamon sugar. Enjoy!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Sweet and Savory Meatballs</h2>
+
+    <figure>
+        <img class="sweet-savory-meatballs" src="images/sweet-savory-meatballs.jpg" alt="plate of sweet and savory meatballs">
+        <figcaption>
+            Ed Taylor
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 can of jellied cranberry sauce</li>
+        <li>1 12oz bottle of chili sauce</li>
+        <li>1 small bag of Italian style frozen meatballs (can be homemade, but MUST be frozen)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Empty can of jellied cranberry sauce and bottle of chili sauce into medium sized saucepan, on medium-low heat, stirring frequently until cranberry sauce becomes becomes liquid.</li>
+        <li class="directions">Add frozen meatballs to sauce mixture and stir, coating meatballs evenly.</li>
+        <li class="directions">Let simmer until meatballs have thawed, and cooked through.</li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
+<?php include 'footer.php'; ?>
