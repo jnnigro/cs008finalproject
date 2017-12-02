29c3108b33bda593956c60fcc494c548a1f1824c
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:18:10 -0500

    Update dessert-cookies.php

--- dessert-cookies.php
+++ dessert-cookies.php
@@ -1,1 +1,145 @@
 <?php include 'top.php'; ?>
+
+<h1>Cookies</h1>
+
+<article class='recipe-title'>
+    <h2>Cherry Chocolate Biscotti</h2>
+
+    <figure>
+        <img class="biscotti" src="images/biscotti.JPG" alt="tray of cherry chocolate biscotti drizzled with chocolate">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>3/4 cup sugar</li>
+        <li>1/2 cup butter, softened</li>
+        <li>2 tsp almond extract</li>
+        <li>3 eggs</li>
+        <li>3 cups all-purpose flour</li>
+        <li>2 tsp baking powder</li>
+        <li>1 cup maraschino cherries, paper towel dried</li>
+        <li>1/2 cup miniature chocolate chips</li>
+        <li>1/2 cup chocolate chips, melted (optional)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Heat oven to 350°F. Lightly grease a cookie sheet. Beat sugar and butter on medium speed in a large bowl until well blended. Beat in almond extract and eggs. Stir in flour and baking powder. Stir in cherries and chocolate chips.</li>
+        <li class="directions">Shape dough into two 10-inch rolls. Place rolls 5 inches apart on the cookie sheet and flatten each to 3 inches in width.</li>
+        <li class="directions">Bake 20-25 minutes or until set and light golden brown. Cool 10 minutes, then gently cut diagonally with a serrated knife into 1/2 inch slices. Turn the slices cut sides down on an oven safe cooling rack. Bake 10-15 minutes longer or until browned and crisp.</li>
+        <li class="directions">Cool completely on the cooling rack. Then, if desired, drizzle the biscotti with melted chocolate.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Classic Nestlé® Toll House® Chocolate Chip Cookies</h2>
+
+    <figure>
+        <img class="choc-chip-cookies" src="images/choc-chip-cookies.JPG" alt="box of chocolate chip cookies">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 1/4 cups all-purpose flour</li>
+        <li>1 tsp baking soda</li>
+        <li>1 tsp salt</li>
+        <li>1 cup (2 sticks) butter, softened</li>
+        <li>3/4 cup granulated sugar</li>
+        <li>3/4 cup packed brown sugar</li>
+        <li>1 tsp vanilla extract</li>
+        <li>2 large eggs</li>
+        <li>2 cups (12-oz. pkg.) NESTLÉ® TOLL HOUSE® Semi-Sweet Chocolate Morsels</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 375° F. For a softer cookie, preheat oven to 350°F.</li>
+        <li class="directions">Combine flour, baking soda and salt in small bowl. Beat butter, granulated sugar, brown sugar and vanilla extract in large mixer bowl until creamy. Add eggs, one at a time, beating well after each addition. Gradually beat in flour mixture. Stir in chocolate chips. Drop by rounded tablespoon or cookie/ice cream scoop onto ungreased baking sheets.</li>
+        <li class="directions">Bake for 9 to 11 minutes or until golden brown. Cool on baking sheets for 2 minutes; remove to wire racks to cool completely.</li>
+</article>
+
+<article class="recipe-title">
+    <h2>Fig Macarons</h2>
+
+    <figure>
+        <img class="figmac" src="images/fig-macarons.JPG" alt="cookie, cream, chocolate">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>300g blanched slivered almonds or almond flour</li>
+        <li>300g powdered sugar</li>
+        <li>300g granulated sugar</li>
+        <li>75g water</li>
+        <li>220g aged egg whites divided</li>
+        <li>1 cup chocolate chips</li>
+        <li>1 cup heavy cream</li>
+        <li>Fig jam</li>
+        <li>Gold shimmer dust</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">If you’re using blanched slivered almonds, add them to a food processor with some of the powdered sugar and grind into a flour. Sift it as you go and keep grinding until you only have about a tablespoon left. If you’re starting with almond flour, there’s not need to grind it further. Add the almond flour and powdered sugar to a food processor and process until they’re combined. Alternatively, you can sift them together into a large bowl at least three times.</li>
+        <li class="directions">In a large bowl, add the first portion of aged egg whites and mix it together until it forms a dough-like paste. Cover with plastic wrap so it doesn’t dry out and set it aside. To age the egg whites, measure out the eggs in a container that’s wiped down with lemon juice or vinegar, cover with plastic wrap, poke holes in the top, and keep them in the fridge for at least a day (the longer the better). When it comes time to use them, make sure they return to room temperature before using them. Using aged egg whites will give you a smoother meringue and a smoother final product.</li>
+        <li class="directions">Add the granulated sugar and water to a small saucepan and use a candy thermometer to keep an eye on the temperature. Bring it to a boil on medium heat and stir to mix everything together. Every once in a while, wipe down the sides with a wet pastry brush to avoid crystallization on the sides of the saucepan. When the syrup reaches 115°C, add the second portion of egg whites to a stand mixer and whisk on medium/high speed.</li>
+        <li class="directions">When the syrup reaches 118°C, pour it very slowly into the egg whites. When the egg whites reach stiff peaks, stop whisking.</li>
+        <li class="directions">Add about a third of the meringue into the almond flour and sugar dough and mix with a spatula until well combined. Keep mixing in the meringue a third at a time. With the last third, fold around the mixture and through the middle. Fold until the batter falls off the spatula in ribbons and disappears back into the batter, in about 10 seconds.</li>
+        <li class="directions">Add the batter to a piping bag fitted with a large round tip. Don’t add too much to the piping bag at one time.</li>
+        <li class="directions">Add dots of batter to the corners of a baking tray and line with parchment paper. Pipe the batter in rounds about an inch and a half in diameter. Space them around a half an inch apart. Tap the tray a few times on the counter to knock out any air bubbles.</li>
+        <li class="directions">Let them sit for at least 30 minutes until a skin forms and they don’t feel sticky. Preheat the oven to 180°C or 355°F.</li>
+        <li class="directions">Bake for 12 minutes and let them cool completely before touching them.</li>
+        <li class="directions">To make the ganache, heat the heavy cream until almost boiling and pour over the chocolate chips. Wait a few minutes, then stir until smooth. Chill for 30 minutes or until it’s thickened enough to stay on the macaron. When it’s cool, add to a piping bag. Add the fig jam to a piping bag, making sure there aren’t any big pieces of fig.</li>
+        <li class="directions">Pipe the ganache and the fig jam on the macarons and sandwich them. Top with gold shimmer dust (optional) or drizzle melted chocolate on top. The macarons will keep in the fridge for a week or so in an airtight container. Enjoy!</li>
+    </ol>
+</article>
+
+<article class="recipe-title">
+    <h2>Rose Macarons</h2>
+
+    <figure>
+        <img class="rosemac" src="images/rose-macarons.jpg" alt="macaron with rose background">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>117g powdered sugar</li>
+        <li>71 g almond flour</li>
+        <li>2 aged egg whites at room temperature</li>
+        <li>53g granulated sugar</li>
+        <li>1 stick butter</li>
+        <li>1 1/2 cups sifted powdered sugar</li>
+        <li>1 teaspoon vanilla extract</li>
+        <li>rose syrup to taste</li>
+        <li>2 tablespoons milk</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Using a digital scale, measure out 117 grams of powdered sugar and 71 grams of almond flour and add it to a food processor or a separate bowl. If using a food processor, pulse until everything is sifted together. If using only a sieve, sift everything through 3 times. When there’s just under 2 tablespoons left, discard those 2 tablespoons.</li>
+        <li class="directions">Preheat the oven to 350°F. For the cookie batter, wipe down your bowl with vinegar or lemon juice to ensure there is no fat or oil anywhere on the bowl. Then, add 2 aged egg whites, around 90 grams. To age your egg whites, make sure the bowl they are ageing in is also wiped down with vinegar or lemon juice. Separate the eggs, cover the whites with plastic wrap, poke some holes in it, and keep it in the refrigerator for 24 hours (and up to a week). Make sure the egg whites are room temperature before using, as they gain more volume at room temperature.</li>
+        <li class="directions">Add 53 grams of granulated sugar, and stir it with the whisk attachment before letting the stand mixer do the rest of the work.</li>
+        <li class="directions">Whisk the egg whites and sugar at medium speed for 2 minutes. Then, increase the speed to medium high and whisk for another 2 minutes. Finally, increase the speed to high and whisk for another 2 minutes until stiff peaks form.</li>
+        <li class="directions">Add the almond flour and powdered sugar mixture to the meringue, and using a rubber spatula, fold it into the egg whites in the technique called macaronage. Go around the sides and through the middle and repeat until the batter reaches the ribbon stage, sinking back into the rest of the batter after around 10 seconds.</li>
+        <li class="directions">Transfer the batter to a piping bag with a ¾ inch round tip. Line a baking sheet with parchment paper (this is very important, as no fats or oils should touch the cookies, so never use cooking spray), and to secure the parchment paper, pipe a small amount of batter on each corner and press down with the parchment paper. Pipe the batter in rounds ¾ inch in diameter, an inch apart on the baking sheet.</li>
+        <li class="directions">Tap the baking sheet against the counter 3 times, releasing any air bubbles. If the batter isn’t flat, flatten any points before baking. Bake at 350°F for 13 minutes, turning the sheet halfway through. Most recipes require the cookies to sit for at least 30 minutes, but with this recipe they can go right into the oven.</li>
+        <li class="directions">After taking the cookies out of the oven, let them cool completely and don’t touch them for at least 10 minutes, as they are very fragile.</li>
+        <li class="directions">To make the frosting, cream one stick of butter. Slowly add the sifted 1 1/2 cups powdered sugar with the mixer on its lowest setting. Add the vanilla and milk, then add rose syrup to taste. Rose syrup is very concentrated, so be careful when adding it. Taste as you go, and add less than you think you’ll need, as the flavors are stronger when you pipe on the frosting. Continue to beat the frosting until it reaches a desired texture, adding small amounts of milk if needed. Transfer to a piping bag with a small star tip. If you don’t have a star tip, you can use any small tip or no tip at all.</li>
+        <li class="directions">Using an offset spatula or a palette knife, gently slide under the cookies. They should come off easily. Pipe the buttercream onto a cookie, then sandwich it with another cookie. If you’d like, pipe a small swirl on the top for decoration.</li>
+        <li class="directions">These macarons will keep for 3-5 days in the refrigerator as long as they’re sandwiched, and will get an even softer texture. Enjoy!</li>
+    </ol>
+</article>
+
+<?php include 'footer.php'; ?>
