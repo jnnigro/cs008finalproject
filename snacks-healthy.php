569ea546d48e752716ba3442b9908ca82ca22efd
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:21:33 -0500

    Update snacks-healthy.php

--- snacks-healthy.php
+++ snacks-healthy.php
@@ -1,1 +1,96 @@
 <?php include 'top.php'; ?>
+
+<h1>Healthy Snacks</h1>
+
+<article class='recipe-title'>
+    <h2>Irish Soda Bread</h2>
+
+    <figure>
+        <img class="soda-bread" src="images/soda-bread.jpg" alt="loaf of Irish soda bread">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>450g strong white flour</li>
+        <li>1 tsp baking soda</li>
+        <li>1 tsp salt</li>
+        <li>300ml buttermilk or 150ml milk mixed with 150ml plain yogurt</li>
+        <li>6 tbsp tepid water</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat the oven to 200°C (400°F). Lightly grease a baking sheet.</li>
+        <li class="directions">Mix together the dry ingredients. Add the buttermilk and enough tepid water to form a very soft dough. I've found using your hands is the easiest way to do this.</li>
+        <li class="directions">Turn out the dough onto a lightly floured surface and shape into a round loaf around 7in in diameter.</li>
+        <li class="directions">Transfer the loaf onto the baking sheet and cut a shallow cross on the top with a sharp knife.</li>
+        <li class="directions">Bake for 30 minutes. After 30 minutes, Turn the loaf upside down and bake for another 10-15 minutes. When it's done, it will sound hollow when you tap it. Cool on a wire rack.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Peanut Butter and Pumpkin Dog Biscuits</h2>
+
+    <figure>
+        <img class="dog-biscuits" src="images/dog-biscuits.jpg" alt="top left: round, star, and flower shaped biscuits. top right: biscuit in front of dog. bottom left: dog biting for biscuit. bottom right: dog eating biscuit.">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2/3 cup pumpkin puree</li>
+        <li>1/4 cup peanut butter</li>
+        <li>2 large eggs</li>
+        <li>3 cups flour</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 350°F. Line a baking sheet with parchment paper and set aside.</li>
+        <li class="directions">Beat pumpkin puree, peanut butter and eggs on medium-high until well combined, about 1-2 minutes. Gradually add 2 1/2 cups flour at low speed, beating just until incorporated. Add an additional 1/4 cup flour at a time just until the dough is no longer sticky.</li>
+        <li class="directions">On a lightly floured surface, knead the dough a few times until it comes together. Using a rolling pin, roll the dough to 1/4-inch thickness. Using cookie cutters, cut out desired shapes and place onto the baking sheet.</li>
+        <li class="directions">Bake 20-25 minutes until the edges are golden brown.</li>
+        <li class="directions">Let cool completely and enjoy a snack with your dog!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Rainbow Smoothie Pops</h2>
+
+    <figure>
+        <img class="smoothie-pops" src="images/rainbow-pops.jpg" alt="rainbow smoothie pops surrounded by fruit">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 1/2 cups plain or vanilla yogurt divided</li>
+        <li>1/2 cup strawberries</li>
+        <li>1/2 cup raspberries</li>
+        <li>1 cup oranges</li>
+        <li>1 cup pineapple</li>
+        <li>1 cup bananas</li>
+        <li>1 cup spinach</li>
+        <li>1 cup mixed berries</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">For the red layer: Add 1/2 cup yogurt, strawberries, and raspberries to a blender and blend until smooth. If you don't have a powerful blender and are using frozen fruit, chop them into smaller pieces before blending. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the orange layer: Add 1/2 cup yogurt and chopped oranges to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the yellow layer: Add 1/2 cup yogurt and chopped pineapple to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the green layer: Add 1/2 cup yogurt, bananas, and spinach to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">For the purple layer: Add 1/2 cup yogurt and mixed berries (I used a mix of raspberries, blackberries, and blueberries) to a blender and blend until smooth. Transfer to a cup and refrigerate.</li>
+        <li class="directions">To assemble, spoon each color into each popsicle mold. I started with red and finished with purple, but you can go in any order. If you have molds with reusable caps with sticks, put them on and freeze the popsicles for 5 hours. If you're using wooden popsicle sticks, freeze for 1-2 hours, add the stick, and freeze for the remaining 3-4 hours.</li>
+        <li class="directions">After 5 hours, run the popsicle mold under warm water and carefully remove the popsicle. Enjoy!</li>
+    </ol>
+</article>
+
+<?php include 'footer.php'; ?>
