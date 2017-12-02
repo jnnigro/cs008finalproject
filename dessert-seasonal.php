f942e45df0f42ef015ed9db2942b11b5d4c5a888
jnnigro <33505386+jnnigro@users.noreply.github.com>
2017-12-02 18:20:04 -0500

    Update dessert-seasonal.php

--- dessert-seasonal.php
+++ dessert-seasonal.php
@@ -1,1 +1,196 @@
 <?php include 'top.php'; ?>
+
+<h1>Seasonal Desserts</h1>
+
+<article class='recipe-title'>
+    <h2>Ben and Jerry's Hot Cocoa</h2>
+
+    <figure>
+        <img class="hot-cocoa" src="images/hot-cocoa.jpg" alt="glass of hot cocoa next to pint of ice cream">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1/2 cup Ben & Jerry's ice cream</li>
+        <li>1/2 cup milk</li>
+        <li>Whipped cream</li>
+        <li>Chocolate shavings (optional)</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Scoop about a 1/2 cup of your favorite Ben & Jerry's ice cream into a frothing pitcher.</li>
+        <li class="directions">Melt the ice cream in the microwave, about 1 minute. Scoop out any big chunks of mix-ins and use them for decoration later.</li>
+        <li class="directions">If you're using a frothing pitcher like I did, fill the milk up to the maximum fill line. If you're using a microwave-safe container, add around a half cup. If you're making a vegan version, use almond milk.</li>
+        <li class="directions">Press the on button to steam and froth. If you're using a microwave, microwave on high for 3 minutes and stir. When it's done, pour into a glass and top with whipped cream, shaved chocolate, and a mix-in (I used a brownie chunk). Enjoy!</li>
+    </ol>
+</article>
+
+<article class="recipe-title">
+    <h2>Chocolate Cupcakes with Coconut Grass and Chocolate Covered Strawberry Footballs</h2>
+
+    <figure>
+        <img class="footballcupcakes" src="images/football-cupcakes.JPG" alt="Chocolate Cupcakes with Coconut Grass and Chocolate Covered Strawberry Footballs">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>100g all-purpose flour</li>
+        <li>20g cocoa powder</li>
+        <li>140g sugar</li>
+        <li>1 1/2 teaspoon baking powder</li>
+        <li>pinch salt</li>
+        <li>40g unsalted butter at room temperature</li>
+        <li>120ml milk</li>
+        <li>1 egg</li>
+        <li>1/4 tsp vanilla extract</li>
+        <li>1 stick butter</li>
+        <li>1 1/2 cup sifted powdered sugar</li>
+        <li>1 tsp vanilla extract</li>
+        <li>2 tbsp milk</li>
+        <li>12 strawberries</li>
+        <li>2/3 cup chocolate chips</li>
+        <li>1/2 cup white chocolate chips</li>
+        <li>2/3 cup shredded coconut</li>
+        <li>Green food dye</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Melt the chocolate chips in the microwave at 30 second intervals until smooth. Dip the strawberries in chocolate for a smooth, even coat. Place on a baking tray lined with parchment paper and chill for 15 minutes.</li>
+        <li class="directions">Melt the white chocolate chips in the same fashion and transfer to a piping bag. Snip the end and pipe laces on the strawberry “footballs.” Chill until it’s time to top the cupcakes.</li>
+        <li class="directions">Preheat the oven to 170°C or 325°F. Add the flour, cocoa powder, sugar, baking powder, salt and butter in a mixing bowl. Beat until everything is combined and has a sandy texture.</li>
+        <li class="directions">Whisk the milk, egg and vanilla extract together in a separate bowl, then slowly pour it into the dry ingredients and beat until everything is combined. Go in with a spatula to get anything at the bottom not mixed in and beat for another 20 seconds.</li>
+        <li class="directions">Spoon the batter into a lined cupcake tin until 2/3 full. Bake for 20-25 minutes until a toothpick inserted in the center comes out clean. Let the cupcakes cool slightly in the tin and transfer them to a wire rack to cool completely.</li>
+        <li class="directions">While the cupcakes are baking, add the shredded coconut to a bowl and add a few drops of green food dye. Stir the coconut until it’s all green.</li>
+        <li class="directions">While the cupcakes are cooling, prepare the frosting. Add the butter to a mixing bowl and beat it until it’s creamy. Reduce the speed of the mixer and slowly add the sifted powdered sugar. Once it looks more like frosting, add the vanilla. Gradually add the milk until the frosting is a desired texture.</li>
+        <li class="directions">When the cupcakes are completely cool, frost the cupcakes. You could do this with a piping bag or just with a palette knife. Immediately after frosting, dunk the cupcake frosting-side down into the coconut “grass.”</li>
+        <li class="directions">Top with a chocolate covered strawberry “football” and enjoy!</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Graveyard Cupcakes</h2>
+
+    <figure>
+        <img class="graveyard-cupcakes" src="images/graveyard-cupcake.JPG" alt="chocolate cupcake with shortbread gravestone in crushed cookie dirt">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Halloween Sprinkle-Filled Cupcakes</h2>
+
+    <figure>
+        <img class="sprinkle-cupcakes" src="images/sprinkles-cupcakes.JPG" alt="chococolate cupcakes filled with halloween sprinkles">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+        <li class="directions"></li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Pumpkin Pie</h2>
+
+    <figure>
+        <img class="pumpkin-pie" src="images/pumpkin-pie.jpg" alt="pumpkin pie">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>4 eggs</li>
+        <li>29oz pumpkin purée</li>
+        <li>1 1/2 cups sugar</li>
+        <li>1 tsp salt</li>
+        <li>2 tsp cinnamon</li>
+        <li>1 tsp ginger</li>
+        <li>1 tsp allspice</li>
+        <li>2 cups milk</li>
+        <li>2 pie crusts</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 400°F. Mix beaten eggs, pumpkin, sugar, salt, and spices until well combined. Add milk and stir until mixture is smooth.</li>
+        <li class="directions">Line two 9-inch pie pans with pie crust or use pre-made crusts. Pur the filling into the pastry shells, dividing it equally.</li>
+        <li class="directions">Bake 45 minutes or until a knife inserted halfway between center and edge comes out clean.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Pumpkin Roll</h2>
+
+    <figure>
+        <img class="pumpkin-roll" src="images/pumpkin-roll.jpg" alt="sliced pumpkin roll on a plate next to an unsliced roll">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>Powdered Sugar</li>
+        <li>3/4 cup all purpose flour</li>
+        <li>1/2 tsp baking soda</li>
+        <li>1 tsp ground cinnamon</li>
+        <li>1/4 tsp salt</li>
+        <li>3 large eggs</li>
+        <li>1 cup sugar</li>
+        <li>2/3 cup pumpkin purée</li>
+        <li>1 pkg (8oz.) cream cheese, softened</li>
+        <li>1 cup sifted powdered sugar</li>
+        <li>6 tbsp butter, softened</li>
+        <li>1 tsp vanilla extract</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">For the Cake: Preheat oven to 375°F. Grease a 15x10 inch jelly-roll pan and line with wax paper. Grease and flour the paper. Sprinkle a thin, cotton, kitchen towel with powdered sugar.</li>
+        <li class="directions">Combine flour, baking powder, baking soda, cinnamon, and salt in a small bowl. Beat eggs and sugar in a large mixer bowl until thick. Beat in pumpkin. Stir in the flour mixture. Spread evenly into the prepared pan.</li>
+        <li class="directions">Bake for 13-15 minutes or until top of cake springs back when touched. Immediately loosen and turn the cake onto the prepared towel. Carefully peel off the wax paper. Roll up the cake and towel together, starting with the narrow end. Cool on a wire rack.</li>
+        <li class="directions">For the Filling: Beat cream cheese, powdered sugar, butter, and vanilla extract in a small mixer bowl until smooth. Carefully unroll the cake and remove the towel. Spread the cream cheese mixture over the cake and reroll the cake. Wrap in plastic wrap and refrigerate at least one hour.</li>
+    </ol>
+</article>
+
+<?php include 'footer.php'; ?>
