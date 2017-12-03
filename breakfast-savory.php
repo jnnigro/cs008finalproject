e33092cf1c0145225afe9dce28e5f4ff36e81cb5
ertaylor <33701359+ertaylor@users.noreply.github.com>
2017-12-02 20:51:56 -0500

    Update breakfast-savory.php

--- breakfast-savory.php
+++ breakfast-savory.php
@@ -1,2 +1,31 @@
 <?php include 'top.php'; ?>
 <h1>Savory</h1>
+
+<article class='recipe'>
+    <h2>Rise and Shiner</h2>
+
+    <figure>
+        <img class="" src="images/FILENAME.JPG" alt="">
+        <figcaption>
+            PHOTO CREDIT HERE
+        </figcaption>
+    </figure>
+
+    <h3 class="ingredients">Ingredients</h3>
+    <ul>
+        <li>1 Bagel</li>
+        <li>3 strips of bacon and/or 1 sausage patty and/or 2 slices of ham</li>
+        <li>1 egg</li>
+        <li>1 hashbrown</li>
+        <li>1 slice of cheese</li>
+    </ul>
+
+    <h3 class="instructions">Instructions</h3>
+    <ol>
+        <li class="directions">Toast bagel</li>
+        <li class="directions">Prepare meat(s) according to their instructions</li>
+        <li class="directions">Cook egg sunny side up (see our recipe)</li>
+        <li class="directions">Prepare hash brown according to instructions</li>
+        <li class="directions">Assemble sandwich in the order of: bagel half, meat(s), cheese, egg, hash brown, bagel half.</li>
+    </ol>
+</article>
