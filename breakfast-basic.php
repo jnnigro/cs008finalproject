7aefa4f0d4e7d8ae59b3ac6bf15446c1ab627c50
ertaylor <33701359+ertaylor@users.noreply.github.com>
2017-12-02 20:03:02 -0500

    Update breakfast-basic.php

--- breakfast-basic.php
+++ breakfast-basic.php
@@ -1,1 +1,115 @@
 <?php include 'top.php'; ?>
+
+<h2>French Toast</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        French Toast served with strawberries, blueberries, blackberries, watermelon, and mint leave. (Taken by Jennifer Nigro)
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>1 egg</li>
+    <li>1 teaspoon of vanilla extract</li>
+    <li>1/2 teaspoon of ground cinnamon</li>
+    <li>1/4 cup of milk</li>
+    <li>4 slices of bread</li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions">Beat egg, vanilla and cinnamon in shallow dish. Stir in milk.</li>
+    <li class="directions">Dip bread in egg mixture, turning to coat both sides evenly.</li>
+    <li class="directions">Cook bread slices on lightly greased nonstick griddle or skillet on medium heat until browned on both sides.</li>
+</ol>
+
+
+<h2>Eggs Sunny Side Up</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        Sunny side up eggs served with toast and sauteed spinach. (Taken by Jennifer Nigro)
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>1 tablespoon of vegetable oil (or butter)</li>
+    <li>2 large eggs</li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions">Heat a large, non-stick frying pan over medium-low heat.</li>
+    <li class="directions">Lightly coat the bottom of the pan with any vegetable oil of your preference or butter</li>
+    <li class="directions">Crack the eggs gently into the pan to keep the yolks intact </li>
+    <li class="directions">Cook until the tops of the whites are set but the yolk is still runny. This will take about 2 to 2-1/2 minutes. </li>
+    <li class="directions">When sunny-side up eggs are ready, remove the pan from the heat. Use a large, metal spatula (not rubber) to remove the eggs from the pan one at a time and place on a serving plate. </li>
+
+</ol>
+
+
+<h2>Oatmeal Bowl</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        An oatmeal bowl topped with strawberries, mango, Nutella, and tea biscuits. (Taken by Jennifer Nigro)
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>1/2 cup of oats</li>
+    <li>1 cup of water or milk</li>
+    <li>Dash of salt (optional)</li>
+</ul>
+
+<h3>Instructions for Stovetop</h3>
+<ol class="instructions">
+    <li class="directions">Boil water or milk and salt</li>
+    <li class="directions">Stir in oats</li>
+    <li class="directions">Cook about 5 minutes over medium heat; stir occasionally</li>
+</ol>
+
+<h3>Instructions for Microwave</h3>
+<ol class="instructions">
+    <li class="directions">Combine water or milk, salt and oats in a medium microwave-safe bowl</li>
+    <li class="directions">Microwave on HIGH 2 1/2 to 3 minutes; stir before serving</li>
+</ol>
+
+<h2>Chocolate Chip Pancakes</h2>
+
+<figure>
+    <img class="" src="images/FILENAME.JPG" alt="">
+    <figcaption>
+        Chocolate Chip Pancakes. (Taken by Jennifer Nigro)
+    </figcaption>
+</figure>
+
+<h3>Ingredients</h3>
+<ul class="ingredients">
+    <li>4 tablespoons (1/2 stick) unsalted butter</li>
+    <li>1 cup whole, 2 percent fat, or 1 percent fat milk</li>
+    <li>1 1/4 cups flour</li>
+    <li>1 tablespoon sugar</li>
+    <li>4 teaspoons baking powder</li>
+    <li>3/4 teaspoon salt</li>
+    <li>2 eggs</li>
+    <li>6 ounces semisweet chocolate chips, or less to taste</li>
+    <li>Butter, for cooking</li>
+</ul>
+
+<h3>Instructions</h3>
+<ol class="instructions">
+    <li class="directions">In a small saucepan, combine the butter and milk. Place over low heat just until warm and the butter is melted. Let cool slightly.</li>
+    <li class="directions">In a bowl, combine the flour, sugar, baking powder, and salt; mix well.</li>
+    <li class="directions">In a large bowl, whisk the eggs with a fork. Whisk in the milk mixture.</li>
+    <li class="directions">Add the dry ingredients and mix just until barely blended. Add the chocolate chips and mix. </li>
+    <li class="directions">Heat a griddle or large skillet over medium heat. Add about 1 teaspoon of butter and melt until bubbly.</li>
+    <li class="directions">Ladle 3 tablespoons of batter for each pancake onto the hot surface and cook until bubbly on the top and golden brown on the bottom.</li>
+    <li class="directions">Turn and cook until golden brown on the other side, about 30 seconds more. Repeat until all the batter is used up. Serve hot.</li>
+</ol>
