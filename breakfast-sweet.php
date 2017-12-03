6afedae9a559f0a2acf73cf55adfddce71ba42b9
ertaylor <33701359+ertaylor@users.noreply.github.com>
2017-12-02 21:46:17 -0500

    Update breakfast-sweet.php

--- breakfast-sweet.php
+++ breakfast-sweet.php
@@ -1,2 +1,59 @@
 <?php include 'top.php'; ?>
 <h1>Sweet</h1>
+
+<article class='recipe'>
+    <h2>Apple Pie Overnight Oats</h2>
+
+    <figure>
+        <img class="" src="images/FILENAME.JPG" alt="">
+        <figcaption>
+            PHOTO CREDIT HERE
+        </figcaption>
+    </figure>
+
+    <h3 class="ingredients">Ingredients</h3>
+    <ul>
+        <li>1/2 cup of rolled oats</li>
+        <li>1/2 chopped Fuji apple</li>
+        <li>1/2 cup of almond milk</li>
+        <li>1 tablespoon of chia seeds</li>
+        <li>1 teaspoon of cinnamon</li>
+        <li>1/2 teaspoon of vanilla extract</li>
+        <li>1/4 cup of chopped walnuts</li>
+        <li>2 teaspoons of maple syrup</li>
+    </ul>
+
+    <h3 class="instructions">Instructions</h3>
+    <ol>
+        <li class="directions">Core and dice half the apple into small pieces. In a jar with a lid, layer only 1/2 cup of both the oats and almond milk (you will have extra). Layer diced apple, Chia seeds, cinnamon, vanilla, walnuts and maple syrup. Cover and refrigerate overnight.</li>
+        <li class="directions">Wake up and enjoy</li>
+    </ol>
+</article>
+
+<article class='recipe'>
+    <h2>Peanut Butter and Jelly Overnight Oats</h2>
+
+    <figure>
+        <img class="" src="images/FILENAME.JPG" alt="">
+        <figcaption>
+            PHOTO CREDIT HERE
+        </figcaption>
+    </figure>
+
+    <h3 class="ingredients">Ingredients</h3>
+    <ul>
+        <li>2/3 cup of steel-cut oats</li>
+        <li>2/3 cup of milk</li>
+        <li>2 tablespoons of peanut butter</li>
+        <li>Dried fruit as desired</li>
+        <li>2 tablespoons of jam</li>
+        <li>Granola as desired</li>
+    </ul>
+
+    <h3 class="instructions">Instructions</h3>
+    <ol>
+        <li class="directions">Add the oats, milk, peanut butter, and dried fruit to a mason jar or other airtight container. If you want to add additional sweetener, add it here.</li>
+        <li class="directions">Refrigerate overnight</li>
+        <li class="directions">Add jam and granola and enjoy</li>
+    </ol>
+</article>
