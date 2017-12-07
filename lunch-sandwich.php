ac9da9e224c1e898c7998510167dbac747a0681b
ertaylor <33701359+ertaylor@users.noreply.github.com>
2017-12-07 15:29:37 -0500

    Update lunch-sandwich.php

--- lunch-sandwich.php
+++ lunch-sandwich.php
@@ -1,1 +1,56 @@
 <?php include 'top.php'; ?>
+
+<article class='recipe-title'>
+    <h2>Pesto Chicken Wrap</h2>
+
+    <figure>
+        <img class="chicken-wrap" src="images/chicken-wrap.jpg" alt="a pesto chicken wrap">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 Tortilla Wrap</li>
+        <li>2 oz of Pesto</li>
+        <li>1 packet of Mayonnaise</li>
+        <li>2 slices of Mozzarella Cheese</li>
+        <li>2 oz of Roasted red cherry tomatoes</li>
+        <li>5 oz of Sous vide cooked chicken</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Lay tortilla flat and evenly spread the pesto and mayonnaise over the tortilla leaving a 2 inch border</li>
+        <li class="directions">Lay the cheese and tomatoes. Hand tear the chicken into pieces and lay over the tomatoes.</li>
+        <li class="directions">Slightly fold two sides over and roll the tortilla to close the wrap.</li>
+    </ol>
+</article>
+
+<article class='recipe-title'>
+    <h2>Turkey Breast and Havarti Cheese Wrap</h2>
+
+    <figure>
+        <img class="turkey-wrap" src="images/turkey-wrap.jpg" alt="a turkey havarti wrap">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 Tortilla wrap (sundried tomato and basil recommended)</li>
+        <li>2 oz of Chipotle bean spread (or chipotle mayo)</li>
+        <li>4 oz of Sliced smoked turkey</li>
+        <li>2 Havarti cheese slices</li>
+        <li>1 oz of Baby spinach</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Lay tortilla flat and evenly spread white bean spread (or mayo) over the tortilla leaving a 2 inch border.</li>
+        <li class="directions">Evenly lay the turkey, cheese and spinach over the spread in that order.</li>
+        <li class="directions">Slightly fold two sides over and roll the tortilla over to close the wrap.</li>
+    </ol>
+</article>
