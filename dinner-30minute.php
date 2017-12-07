369443d596dbc7e7ad68b41c9569fe4ecb25deac
ertaylor <33701359+ertaylor@users.noreply.github.com>
2017-12-07 16:02:38 -0500

    Update dinner-30minute.php

--- dinner-30minute.php
+++ dinner-30minute.php
@@ -1,1 +1,45 @@
 <?php include 'top.php'; ?>
+
+<article class='recipe-title'>
+    <h2>Thai Shrimp Stir Fry</h2>
+
+    <figure>
+        <img class="thai-shrimp" src="images/thai-shrimp.jpg" alt="thai shrimp stir fry">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 Red Onion</li>
+        <li>4 oz of Crimini Mushrooms</li>
+        <li>1.5 oz of Sugar snap peas</li>
+        <li>1 Carrot</li>
+        <li>1 Garlic clove</li>
+        <li>1/4 oz of Fresh cilantro</li>
+        <li>1 Lime</li>
+        <li>6 oz of Pad thai noodles</li>
+        <li>4 oz of Bamboo shoots</li>
+        <li>1 oz of Roasted corn</li>
+        <li>12 Medium shrimp</li>
+        <li>1 tbsp of Fish sauce</li>
+        <li>1 tbsp of Sriracha sauce</li>
+        <li>1 cup of Coconut Milk</li>
+    </ul>
+
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Peel and cut the red onion into 1/4 inch diced pieces, set aside. Remove stems from mushrooms and discard the stems. Slice mushroom caps into 1/2 inch thick slices and set aside</li>
+        <li class="directions">Cut the snap peas in half at a diagonal and set aside. Peel the carrot and make an angled cut. Finely chop the garlic and set aside.</li>
+        <li class="directions">Remove the cilantro leaves from the stem and roughly chop the leaves. Cut limes into wedges, set aside for garnish.</li>
+        <li class="directions">Bring 4 cups of water to a boil in a medium sauce pot over high heat. Once the water boils, remove from heat, add Pad Thai noodles and let them sit for 8-10 minutes until they are soft but firm.</li>
+        <li class="directions">Drain the noodles in a colander and rinse with cold water.</li>
+        <li class="directions">Add a tablespoon of vegetable oil to a large saute pan over medium high heat. Drain only HALF the bamboo shoots and set aside.</li>
+        <li class="directions">Add onions, snap peas, mushrooms, carrots, garlic, bamboo shoots, roasted corn, and only HALF the cilantro.</li>
+        <li class="directions">Stir to combine and cook vegetables for 6 to 8 minutes until softened</li>
+        <li class="directions">Pat dry the shrimp with paper towels and season with a 1/4 teaspoon each of salt and pepper. Add the shrimp to the large saute pan with the vegetables and cook for 3-4 minutes until shrimp is cooked through.</li>
+        <li class="directions">Add the fish sauce, sriracha, and coconut milk to the pan. Mix well to combine and coat the shrimp and vegetables. Simmer on low heat until the shrimp is opaque on both sides. Add in Pad Thai noodles and toss to combine.</li>
+        <li class="directions">Garnish the stir fry with the remaining cilantro. Serve with lime wedges on the side.</li>
+    </ol>
+</article>
