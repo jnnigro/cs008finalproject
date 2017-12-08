e20f6bac4ef763bf22eecd66d6d6298c85b85962
bennettcazayoux <33701275+bennettcazayoux@users.noreply.github.com>
2017-12-08 01:40:32 -0500

    Update dinner-30minute.php

--- dinner-30minute.php
+++ dinner-30minute.php
@@ -46,10 +46,62 @@
 <article>
     <h2>Chicken Parm Pizza</h2>
     <figure>
-        <img class="thai-shrimp" src="images/thai-shrimp.jpg" alt="thai shrimp stir fry">
+        <img class="chicken-parm" src="images/cp-pizza.jpg" alt="chicken parm">
         <figcaption>
             Jennifer Nigro
         </figcaption>
     </figure>
     <h3>Ingredients</h3>
     <ul class="ingredients">
+        <li>1 bag of Perdue Short Cuts chicken</li>
+        <li>2 pizza crusts</li>
+        <li>2/3 cup tomato sauce</li>
+        <li>2 cup mozzarella cheese</li>
+        <li>2/3 cup shredded parmesean cheese</li>
+    </ul>
+    
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat over to 400<sup>o</sup>F. Place the pizza crusts on a baking sheet adnd spoon tomato saurce on them</li>
+        <li class="directions">Top with the chicken, breaking apart larger pieces as needed</li>
+        <li class="directions">Add the mozzarella cheese (enough to cover the chicken or to your preference)</li>
+        <li class="directions">Bake for 10 minutes and top with parmesan cheese</li>
+    </ol>
+</article>
+
+<article>
+    <h2>White Castle Sliders</h2>
+    <figure>
+        <img class="wc-sliders" src="images/wc-sliders.jpg" alt="wc sliders">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+    
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 package 16 hamburger buns</li>
+        <li>1 lb ground chuck</li>
+        <li>1 Vidalia Onion</li>
+        <li>Beef broth</li>
+        <li>8 slices mild cheddar or American cheese</li>
+        <li>16 hamburger dill slices</li>
+        <li>Salt</li>
+        <li>Pepepr</li>            
+    </ul>
+    
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Chop the Vidalia Onion</li>
+        <li class="directions">Pour about a quarter inch of beef broth into a large pan and add the onions. Add salt and pepper to taste and cook at medium heat until the broth boils over</li>
+        <li class="directions">Press and spread the chuck until it's a large rectangle, around 10x15 inches</li>
+        <li class="directions">Cut the meat into 16 equal rectangles and poke 5 holes in each patty. Sprinkle salt and pepper to season</li>
+        <li class="directions">Heat a griddle to 400<sup>o</sup>F and spread the onions. Put the patties on the griddle and cook until about halfway done, then flip them</li>
+        <li class="directions">After the patties are flipped, add cheese and let them cook until the cheese it fully melted and the meat is thoroughly cooked.</li>
+        <li class="directions">Cut the sides off the buns and place a dill pickle slice on the bottom bun. Top with a patty, condiments (optional), and the top bun.</li>
+        <li class="directions">Enjoy!</li>        
+    </ol>       
+</article>
+<?php include 'footer.php'; ?>
+  
+            
