4b119d2d6febc41186f5c6aabcce5a3b6383c0ef
bennettcazayoux <33701275+bennettcazayoux@users.noreply.github.com>
2017-12-08 02:08:16 -0500

    Update dinner-comfort.php

--- dinner-comfort.php
+++ dinner-comfort.php
@@ -1,1 +1,84 @@
-<?php include 'top.php'; ?>
+<?php
+include 'top.php';
+?>
+
+<article>
+    <h2>Baked Spaghetti</h2>
+    <figure>
+        <img class="baked-spaghetti" src="images/wc-sliders.jpg" alt="baked spaghetti">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+    
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>2 tablespoons Sultan brand olive oil</li>
+        <li>2 lbs ground lamb</li>
+        <li>1 tablespoon Ziyad brand cumin</li>
+        <li>1 tablespoon Ziyad brand oregano</li>
+        <li>1 tablespoon basil</li>
+        <li>1 onion diced</li>
+        <li>3 cloves minced garlic</li>
+        <li>28oz can crushed tomatoes</li>
+        <li>2 tablespoons Ziyad brand tomato paste</li>
+        <li>1 jar pimentos</li>
+        <li>1.5 lbs spaghetti</li>
+        <li>Salt</li>
+        <li>1 1/4 cup breadcrumbs</li>
+        <li>1 1/2 cup grated Parmesan cheese</li>
+        <li>Sumac to finish</li>
+    </ul>
+    
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Add 2 tablespoons of olive oil to a large skillet with raised sides. When it's hot, add 2 lbs of ground lamb and break it up into smaller pieces. Season with 1 tablespoon cumin, 1 tablespoon oregano, 1 tablespoon basil, and 2 tablespoons parsley. Mix it to ensure everything is combined.</li>
+        <li class="directions">Add 1 diced onion and 3 cloves minced garlic and stir, cooking until the onions have softened.</li>
+        <li class="directions">Add a 28 oz can of crushed tomatoes and 2 tablespoons tomato paste and mix everything together. Add a small jar of pimientos and stir. Simmer for 20 minutes.</li>
+        <li class="directions">Preheat the oven to 350°F. Cook 1 1/2 lbs of spaghetti according to the instructions on the box, cooking until the pasta is al dente. Strain.</li>
+        <li class="directions">Add the spaghetti to a large baking dish. Add the sauce and spread it out to cover the spaghetti. Top with 1/4 cup of breadcrumbs and 1/2 cup (or more) of Parmesan cheese. Bake at 350°F for 20 minutes.</li>
+        <li class="directions">Plate a portion of the spaghetti and sprinkle with sumac. Enjoy!</li>            
+    </ol>                  
+</article>
+
+<article>
+    <h2>Swedish Meatballs</h2>
+    <figure>
+        <img class="swedish-meatballs" src="images/sweet-savory-meatballs.jpg" alt="swedish meatballs">
+        <figcaption>
+            Jennifer Nigro
+        </figcaption>
+    </figure>
+    
+    <h3>Ingredients</h3>
+    <ul class="ingredients">
+        <li>1 small onion, minced</li>
+        <li>2/3 pound ground beef</li>
+        <li>1/3 pound finely ground pork</li>
+        <li>1 egg</li>
+        <li>1 tablespoon brown sugar (optional)</li>
+        <li>1 teaspoon salt</li>
+        <li>1/4 teaspoon ground black pepper</li>
+        <li>1/4 teaspoon ground nutmeg</li>
+        <li>1/4 teaspoon ground allspice</li>
+        <li>1/8 teaspoon ground ginger (optional)</li>
+        <li>1 tablespoon butter</li>
+        <li>1/4 cup chicken broth</li>
+        <li>3 tablespoons all-purpose flour, or as needed</li>
+        <li>2 cups beef broth, or as needed</li>
+        <li>1/2 container sour cream</li>        
+    </ul>
+    
+    <h3>Instructions</h3>
+    <ol class="instructions">
+        <li class="directions">Preheat oven to 350 degrees F (175 degrees C).</li>
+        <li class="directions">Place the bread crumbs into a small bowl, and mix in the cream. Allow to stand until crumbs absorb the cream, about 10 minutes. While the bread is soaking, melt 1 teaspoon of butter in a skillet over medium heat, and cook and stir the onion until it turns light brown, about 10 minutes. Place onion into a mixing bowl; mix with the ground beef, ground pork, egg, brown sugar, salt, black pepper, nutmeg, allspice, and ginger. Lightly mix in the bread crumbs and cream.</li>
+        <li class="directions">Melt 1 tablespoon of butter in a large skillet over medium heat. Pinch off about 1 1/2 tablespoon of the meat mixture per meatball, and form into balls. Place the meatballs into the skillet, and cook just until the outsides are brown, about 5 minutes, turning the meatballs often. Insides of the meatballs will still be pink. Place browned meatballs into a baking dish, pour in chicken broth, and cover with foil.</li>
+        <li class="directions">Bake in the preheated oven until the meatballs are tender, about 40 minutes. Remove meatballs to a serving dish.</li>
+        <li class="directions">To make brown gravy, pour pan drippings into a saucepan over medium heat. Whisk the flour into the pan drippings until smooth, and gradually whisk in enough beef broth to total about 2 1/2 cups of liquid. Bring the gravy to a simmer, whisking constantly until thick, about 5 minutes. Just before serving, whisk in the sour cream. Season to taste with salt and black pepper. Serve the gravy with the meatballs.</li>
+    </ol>
+    
+        
+</article>
+
+<?php include 'footer.php'; ?>
